424d3d0a4587fda2847f8d3f2afefa435dd0a303
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-20 12:34:28 +0100

    Add documentation comments

--- src/Application/ConfigureServices.cs
+++ src/Application/ConfigureServices.cs
@@ -2,8 +2,15 @@
 
 namespace Application;
 
+/// <summary>
+///     The ConfigureServices class
+/// </summary>
 public static class ConfigureServices
 {
+    /// <summary>
+    ///     Adds application project services
+    /// </summary>
+    /// <param name="services">The services</param>
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         return services;
--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -3,8 +3,16 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
 
+/// <summary>
+///     The ConfigureServices class
+/// </summary>
 public static class ConfigureServices
 {
+    /// <summary>
+    ///     Adds infrastructure project services
+    /// </summary>
+    /// <param name="services">The services</param>
+    /// <param name="configuration">The configuration</param>
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
