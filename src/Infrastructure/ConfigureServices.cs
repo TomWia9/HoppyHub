56f23f8b2d91a67a744b04bee54637adc06cbb13
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-20 15:02:01 +0100

    Add DateTimeService

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Services;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
@@ -16,6 +18,8 @@ public static class ConfigureServices
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
+        services.AddTransient<IDateTime, DateTimeService>();
+
         return services;
     }
 }
