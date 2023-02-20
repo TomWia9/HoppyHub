fd6dbbbfb24aa23ab1b903fddf8e724cabd42f3d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-20 16:51:47 +0100

    Add dbContext config to ConfigureServices

--- src/Infrastructure/ConfigureServices.cs
+++ src/Infrastructure/ConfigureServices.cs
@@ -1,6 +1,8 @@
 ﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
 using Infrastructure.Persistence.Interceptors;
 using Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -19,10 +21,15 @@ public static class ConfigureServices
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
         IConfiguration configuration)
     {
-        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
         
-        services.AddTransient<IDateTime, DateTimeService>();
+        services.AddScoped<AuditableEntitySaveChangesInterceptor>();
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
 
+        services.AddTransient<IDateTime, DateTimeService>();
+        
         return services;
     }
 }
