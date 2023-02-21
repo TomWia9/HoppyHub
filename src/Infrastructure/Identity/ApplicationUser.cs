067c30e5a6c5297bb564a6d2c1868db476d17694
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-21 12:31:37 +0100

    Add ApplicationUser and AuthenticationResult classes

---
+++ src/Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Identity;
+
+/// <summary>
+///     The ApplicationUser class. 
+/// </summary>
+public class ApplicationUser : IdentityUser<Guid>
+{
+}
