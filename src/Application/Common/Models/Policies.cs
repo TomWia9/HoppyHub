5d7529095440334dd14a0b121e7df9234d412661
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-21 13:29:21 +0100

    Add roles and policies classes

---
+++ src/Application/Common/Models/Policies.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Common.Models;
+
+/// <summary>
+///     Policies class.
+/// </summary>
+public static class Policies
+{
+    /// <summary>
+    ///     The user access.
+    /// </summary>
+    public const string UserAccess = "UserAccess";
+
+    /// <summary>
+    ///     The administrator access.
+    /// </summary>
+    public const string AdministratorAccess = "AdministratorAccess";
+}
