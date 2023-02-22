446c8958242ae11c4771cc49f4b91fff4706b0fa
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-22 17:35:24 +0100

    Feature/exceptions-filter (#7)
    * Add custom exceptions
    
    * Add ApiExceptionFilterAttribute
    
    * Add ApiExceptionFilterAttributeTests
    
    * Minor fixes

---
+++ src/Application/Common/Exceptions/ValidationException.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation.Results;
+
+namespace Application.Common.Exceptions;
+
+/// <summary>
+///     ValidationException class.
+/// </summary>
+public class ValidationException : Exception
+{
+    /// <summary>
+    ///     Initializes ValidationException.
+    /// </summary>
+    public ValidationException()
+        : base("One or more validation failures have occurred.")
+    {
+        Errors = new Dictionary<string, string[]>();
+    }
+
+    /// <summary>
+    ///     Initializes ValidationException with validation failures.
+    /// </summary>
+    /// <param name="failures"></param>
+    public ValidationException(IEnumerable<ValidationFailure> failures) : this()
+    {
+        Errors = failures
+            .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+            .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+    }
+
+    /// <summary>
+    ///     The errors.
+    /// </summary>
+    public IDictionary<string, string[]> Errors { get; }
+}
