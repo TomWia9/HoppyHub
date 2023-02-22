1bfc9bf389b3e09ecf533cf641b4af1821c9f25f
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-22 13:44:24 +0100

    Add custom exceptions

---
+++ src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Common.Exceptions;
+
+/// <summary>
+///     ForbiddenAccessException class.
+/// </summary>
+public class ForbiddenAccessException : Exception
+{
+    /// <summary>
+    ///     Initializes ForbiddenAccessException.
+    /// </summary>
+    public ForbiddenAccessException() : base() {}
+}
