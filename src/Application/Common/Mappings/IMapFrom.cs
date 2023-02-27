d6efc86f54e01934123db14a250cfe0c8c7787a5
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-27 11:02:27 +0100

    Add IMapFrom interface and MappingProfile

---
+++ src/Application/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace Application.Common.Mappings;
+
+/// <summary>
+///     MapFrom interface.
+/// </summary>
+/// <typeparam name="T">The source type</typeparam>
+public interface IMapFrom<T>
+{
+    /// <summary>
+    ///     Creates a map.
+    /// </summary>
+    /// <param name="profile"></param>
+    void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+}
