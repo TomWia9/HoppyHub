cc5840e3153cc481fc5ec0c8210d122f60d6eeb2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-27 11:19:35 +0100

    Add UserDto class

---
+++ src/Application/Users/Queries/UserDto.cs
@@ -0,0 +1,27 @@
+﻿namespace Application.Users.Queries;
+
+/// <summary>
+///     UserDto class.
+/// </summary>
+public class UserDto
+{
+    /// <summary>
+    ///     The user id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    ///     The email.
+    /// </summary>
+    public string? Email { get; init; }
+
+    /// <summary>
+    ///     The username.
+    /// </summary>
+    public string? Username { get; init; }
+
+    /// <summary>
+    ///     The role.
+    /// </summary>
+    public string? Role { get; init; }
+}
