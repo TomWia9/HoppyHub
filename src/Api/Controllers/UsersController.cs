676f82e334a4acee4298770bc4cf8c942a0116c0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-27 12:11:42 +0100

    Add UsersController with GetUser endpoint

---
+++ src/Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Application.Users.Queries;
+using Application.Users.Queries.GetUser;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+///     The users controller.
+/// </summary>
+public class UsersController : ApiControllerBase
+{
+    /// <summary>
+    ///     Gets user by id.
+    /// </summary>
+    /// <param name="id">The user id</param>
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<UserDto>> GetUser(Guid id)
+    {
+        var result = await Mediator.Send(new GetUserQuery { UserId = id });
+
+        return Ok(result);
+    }
+}
