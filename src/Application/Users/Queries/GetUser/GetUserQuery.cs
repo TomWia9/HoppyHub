c53c7dc9f2eb8eff1700521290b0a98b53f25369
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-02-27 11:51:15 +0100

    Add GetUser query and handler

---
+++ src/Application/Users/Queries/GetUser/GetUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Users.Queries.GetUser;
+
+/// <summary>
+///     GetUser query.
+/// </summary>
+public record GetUserQuery : IRequest<UserDto>
+{
+    /// <summary>
+    ///     The user id.
+    /// </summary>
+    public Guid UserId { get; init; }
+}
