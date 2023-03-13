f0d68b1bf6b078d1c9cec6664d879091b44d7fc0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 17:23:10 +0100

    Add CreateBeer endpoint

---
+++ src/Api/Controllers/BeersController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Beers;
+using Application.Beers.Commands.CreateBeer;
+using Application.Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+///     The beers controller.
+/// </summary>
+public class BeersController : ApiControllerBase
+{
+    /// <summary>
+    ///     Creates beer.
+    /// </summary>
+    /// <param name="command">The CreateBeerCommand</param>
+    /// <returns>An ActionResult of type BeerDto</returns>
+    [Authorize(Policy = Policies.AdministratorAccess)]
+    [HttpPost]
+    public async Task<ActionResult<BeerDto>> CreateBeer([FromBody] CreateBeerCommand command)
+    {
+        var result = await Mediator.Send(command);
+
+        return CreatedAtAction("GetBeer", new { id = result.Id }, result);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<BeerDto>> GetBeer(Guid id)
+    {
+        // TODO Add implementation
+        throw new NotImplementedException();
+    }
+}
