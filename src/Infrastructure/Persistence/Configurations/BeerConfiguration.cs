cd766a10d18b861f8e4e00323b64dcbe9d97e12a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 14:58:08 +0100

    Add entity configuration

---
+++ src/Infrastructure/Persistence/Configurations/BeerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+/// <summary>
+///     The beer entity configuration.
+/// </summary>
+public class BeerConfiguration : BaseConfiguration<Beer>
+{
+    /// <summary>
+    ///     Configures the beer entity.
+    /// </summary>
+    /// <param name="builder">The builder</param>
+    public override void Configure(EntityTypeBuilder<Beer> builder)
+    {
+        base.Configure(builder);
+
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+        builder.Property(x => x.Brewery).IsRequired().HasMaxLength(200);
+        builder.Property(x => x.AlcoholByVolume).IsRequired();
+        builder.Property(x => x.Description).HasMaxLength(3000);
+        builder.Property(x => x.Style).IsRequired().HasMaxLength(50);
+        builder.Property(x => x.Country).IsRequired().HasMaxLength(50);
+    }
+}
