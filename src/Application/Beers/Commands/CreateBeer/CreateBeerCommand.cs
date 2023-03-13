c531d0c60e23061e5139cea02a2b0110c147faf2
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 16:51:26 +0100

    Add CreateBeer command and handler

---
+++ src/Application/Beers/Commands/CreateBeer/CreateBeerCommand.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+
+namespace Application.Beers.Commands.CreateBeer;
+
+public record CreateBeerCommand : IRequest<BeerDto>
+{
+    /// <summary>
+    ///     The beer name.
+    /// </summary>
+    public string? Name { get; set; }
+    
+    /// <summary>
+    ///     The brewery.
+    /// </summary>
+    public string? Brewery { get; set; }
+    
+    /// <summary>
+    ///     The alcohol by volume.
+    /// </summary>
+    public double AlcoholByVolume { get; set; }
+    
+    /// <summary>
+    ///     The beer description.
+    /// </summary>
+    public string? Description { get; set; }
+    
+    /// <summary>
+    ///     The extract in Specific Gravity units.
+    /// </summary>
+    public double? SpecificGravity { get; set; }
+
+    /// <summary>
+    ///     The extract in Balling units.
+    /// </summary>
+    public double? Blg { get; set; }
+
+    /// <summary>
+    ///     The extract in Plato units.
+    /// </summary>
+    public double? Plato { get; set; }
+
+    /// <summary>
+    ///     The beer style.
+    /// </summary>
+    public string? Style { get; set; }
+
+    /// <summary>
+    ///     The International Bitterness Units.
+    /// </summary>
+    public int? Ibu { get; set; }
+
+    /// <summary>
+    ///     The country of origin.
+    /// </summary>
+    public string? Country { get; set; }
+}
