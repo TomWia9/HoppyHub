05b08bf9c6bd372a4f42c54c0b4df2db8c0cd3f6
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 17:51:31 +0100

    Add custom rule for CreateBeerCommandValidator

--- src/Application/Beers/Commands/CreateBeer/CreateBeerCommandValidator.cs
+++ src/Application/Beers/Commands/CreateBeer/CreateBeerCommandValidator.cs
@@ -1,4 +1,6 @@
-﻿using FluentValidation;
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Beers.Commands.CreateBeer;
 
@@ -8,11 +10,18 @@ namespace Application.Beers.Commands.CreateBeer;
 public class CreateBeerCommandValidator : AbstractValidator<CreateBeerCommand>
 {
     /// <summary>
+    ///     The database context.
+    /// </summary>
+    private readonly IApplicationDbContext _context;
+
+    /// <summary>
     ///     Initializes CreateBeerCommandValidator.
     /// </summary>
-    public CreateBeerCommandValidator()
+    /// <param name="context">The database context</param>
+    public CreateBeerCommandValidator(IApplicationDbContext context)
     {
-        RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
+        _context = context;
+
         RuleFor(x => x.Brewery).NotEmpty().MaximumLength(200);
         RuleFor(x => x.AlcoholByVolume).NotNull().InclusiveBetween(0, 100);
         RuleFor(x => x.Description).MaximumLength(3000);
@@ -22,5 +31,22 @@ public class CreateBeerCommandValidator : AbstractValidator<CreateBeerCommand>
         RuleFor(x => x.Style).NotEmpty().MaximumLength(50);
         RuleFor(x => x.Ibu).InclusiveBetween(0, 200);
         RuleFor(x => x.Country).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(200)
+            .MustAsync((beer, name, cancellationToken) =>
+                BeUniqueNameWithinBrewery(name, beer.Brewery, cancellationToken))
+            .WithMessage("The beer name must be unique within the brewery.");
+    }
+
+    /// <summary>
+    ///     The custom rule indicating whether beer name is unique within brewery.
+    /// </summary>
+    /// <param name="name">The beer name</param>
+    /// <param name="brewery">The brewery name</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    private async Task<bool> BeUniqueNameWithinBrewery(string name, string? brewery,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Beers.Where(x => x.Brewery == brewery)
+            .AllAsync(x => x.Name != name.Trim(), cancellationToken);
     }
 }
