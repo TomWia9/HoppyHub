a348af2bccfc52cf03f5fd6986def0f4cfea44b1
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 17:10:25 +0100

    Add CreateBeerCommand validator

---
+++ src/Application/Beers/Commands/CreateBeer/CreateBeerCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace Application.Beers.Commands.CreateBeer;
+
+/// <summary>
+///     CreateBeerCommand validator
+/// </summary>
+public class CreateBeerCommandValidator : AbstractValidator<CreateBeerCommand>
+{
+    /// <summary>
+    ///     Initializes CreateBeerCommandValidator.
+    /// </summary>
+    public CreateBeerCommandValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
+        RuleFor(x => x.Brewery).NotEmpty().MaximumLength(200);
+        RuleFor(x => x.AlcoholByVolume).NotNull().InclusiveBetween(0, 100);
+        RuleFor(x => x.Description).MaximumLength(3000);
+        RuleFor(x => x.SpecificGravity).InclusiveBetween(0, 1.2);
+        RuleFor(x => x.Blg).InclusiveBetween(0, 100);
+        RuleFor(x => x.Plato).InclusiveBetween(0, 100);
+        RuleFor(x => x.Style).NotEmpty().MaximumLength(50);
+        RuleFor(x => x.Ibu).InclusiveBetween(0, 200);
+        RuleFor(x => x.Country).NotEmpty().MaximumLength(50);
+    }
+}
