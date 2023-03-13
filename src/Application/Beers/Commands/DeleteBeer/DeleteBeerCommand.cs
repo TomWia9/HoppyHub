f28ffc08849b4a02e01d5bf32ba1065a26a8f92e
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-13 20:04:05 +0100

    Add DeleteBeer command and handler

---
+++ src/Application/Beers/Commands/DeleteBeer/DeleteBeerCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Beers.Commands.DeleteBeer;
+
+/// <summary>
+///     DeleteBeer command
+/// </summary>
+public record DeleteBeerCommand : IRequest
+{
+    public Guid Id { get; init; }
+}
