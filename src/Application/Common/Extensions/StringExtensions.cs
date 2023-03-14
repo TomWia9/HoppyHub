7e0596f53e7599280b3c45427987f1c4ef9a3695
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-14 16:19:04 +0100

    Add StringExtensions class

---
+++ src/Application/Common/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Application.Common.Extensions;
+
+/// <summary>
+///     The StringExtensions class.
+/// </summary>
+public static class StringExtensions
+{
+    /// <summary>
+    ///     Indicates whether source string contains substring with case insensitive.
+    /// </summary>
+    /// <param name="source">The source string</param>
+    /// <param name="substring">The substring</param>
+    public static bool ContainsCaseInsensitive(this string? source, string substring)
+    {
+        if (string.IsNullOrWhiteSpace(source))
+        {
+            return false;
+        }
+
+        return source.IndexOf(substring, StringComparison.OrdinalIgnoreCase) > -1;
+    }
+}
