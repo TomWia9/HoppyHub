7e8bc5c187abba484c4bc24af4eeefc736f11300
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-15 13:11:57 +0100

    Add unit tests for new endpoints

--- tests/Api.UnitTests/Controllers/BeersControllerTests.cs
+++ tests/Api.UnitTests/Controllers/BeersControllerTests.cs
@@ -3,6 +3,9 @@ using Application.Beers;
 using Application.Beers.Commands.CreateBeer;
 using Application.Beers.Commands.DeleteBeer;
 using Application.Beers.Commands.UpdateBeer;
+using Application.Beers.Queries.GetBeer;
+using Application.Beers.Queries.GetBeers;
+using Application.Common.Models;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 
@@ -14,6 +17,53 @@ namespace Api.UnitTests.Controllers;
 [ExcludeFromCodeCoverage]
 public class BeersControllerTests : ControllerSetup<BeersController>
 {
+    /// <summary>
+    ///     Tests that GetBeers method returns PaginatedList of BeerDto.
+    /// </summary>
+    [Fact]
+    public async Task GetBeers_ShouldReturnPaginatedListOfBeerDto()
+    {
+        // Arrange
+        var beers = new List<BeerDto>
+        {
+            new() { Id = Guid.NewGuid(), Name = "Beer 1" },
+            new() { Id = Guid.NewGuid(), Name = "Beer 2" }
+        };
+        var query = new GetBeersQuery();
+        var expectedResult = PaginatedList<BeerDto>.Create(beers, 1, 10);
+
+        MediatorMock.Setup(m => m.Send(query, CancellationToken.None)).ReturnsAsync(expectedResult);
+
+        // Act
+        var response = await Controller.GetBeers(query);
+
+        // Assert
+        response.Result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeSameAs(expectedResult);
+        Controller.Response.Headers.Should().ContainKey("X-Pagination");
+        Controller.Response.Headers["X-Pagination"].Should().BeEquivalentTo(expectedResult.GetMetadata());
+    }
+
+    /// <summary>
+    ///     Tests that GetBeer method returns BeerDto.
+    /// </summary>
+    [Fact]
+    public async Task GetBeer_ShouldReturnBeerDto()
+    {
+        // Arrange
+        var beerId = Guid.NewGuid();
+        var expectedResult = new BeerDto { Id = beerId, Name = "Test Beer" };
+
+        MediatorMock.Setup(m => m.Send(It.IsAny<GetBeerQuery>(), CancellationToken.None)).ReturnsAsync(expectedResult);
+
+        // Act
+        var response = await Controller.GetBeer(beerId);
+
+        // Assert
+        response.Result.Should().BeOfType<OkObjectResult>()
+            .Which.Value.Should().BeSameAs(expectedResult);
+    }
+
     /// <summary>
     ///     Tests that CreateBeer method returns CreatedAtAction.
     /// </summary>
