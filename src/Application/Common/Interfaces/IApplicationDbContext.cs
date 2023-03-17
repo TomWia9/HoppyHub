e64492c2eb31eadc65245b82a5ed39ea8a4da97e
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-17 09:14:30 +0100

    Add Brewery and Address entities

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -3,9 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Common.Interfaces;
 
+/// <summary>
+///     ApplicationDbContext interface.
+/// </summary>
 public interface IApplicationDbContext
 {
+    /// <summary>
+    ///     The beers.
+    /// </summary>
     DbSet<Beer> Beers { get; }
+    
+    /// <summary>
+    ///     The breweries.
+    /// </summary>
+    DbSet<Brewery> Breweries { get; }
+    
+    /// <summary>
+    ///     The addresses.
+    /// </summary>
+    DbSet<Address> Addresses { get; }
 
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
---
+++ src/Domain/Entities/Brewery.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+/// <summary>
+///     The brewery entity class.
+/// </summary>
+public class Brewery : BaseAuditableEntity
+{
+    /// <summary>
+    ///     The brewery name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    ///     The description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     The foundation year.
+    /// </summary>
+    public int FoundationYear { get; set; }
+
+    /// <summary>
+    ///     The website url.
+    /// </summary>
+    public string? WebsiteUrl { get; set; }
+
+    /// <summary>
+    ///     The brewery address.
+    /// </summary>
+    public Address? Address { get; set; }
+}
