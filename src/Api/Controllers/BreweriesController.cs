c9ea96cf88ee975ae178874b587c06117fd793e7
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-26 11:08:12 +0200

    Add GetBrewery query, handler and endpoint

--- src/Api/Controllers/BreweriesController.cs
+++ src/Api/Controllers/BreweriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Breweries.Dtos;
 using Application.Breweries.Queries.GetBreweries;
+using Application.Breweries.Queries.GetBrewery;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers;
@@ -16,6 +17,19 @@ public class BreweriesController : ApiControllerBase
         var result = await Mediator.Send(query);
 
         Response.Headers.Add("X-Pagination", result.GetMetadata());
+
+        return Ok(result);
+    }
+    
+    /// <summary>
+    ///     Gets brewery by id.
+    /// </summary>
+    /// <param name="id">The brewery id</param>
+    /// <returns>An ActionResult of type BreweryDto</returns>
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<BreweryDto>> GetBeer(Guid id)
+    {
+        var result = await Mediator.Send(new GetBreweryQuery { Id = id });
 
         return Ok(result);
     }
