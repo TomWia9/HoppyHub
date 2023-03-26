cd0e4fe10a5eb31a275b4652d6f1fbf1312f31fb
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-26 09:53:13 +0200

    Add GetBreweriesQuery and BreweriesFilteringHelper

---
+++ src/Application/Breweries/Queries/GetBreweries/GetBreweriesQuery.cs
@@ -0,0 +1,42 @@
+﻿using Application.Breweries.Dtos;
+using Application.Common.Abstractions;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.Breweries.Queries.GetBreweries;
+
+/// <summary>
+///     Get breweries query.
+/// </summary>
+public record GetBreweriesQuery : QueryParameters, IRequest<PaginatedList<BreweryDto>>
+{
+    /// <summary>
+    ///     The brewery name.
+    /// </summary>
+    public string? Name { get; init; }
+    
+    /// <summary>
+    ///     The brewery country.
+    /// </summary>
+    public string? Country { get; init; }
+    
+    /// <summary>
+    ///     The brewery state.
+    /// </summary>
+    public string? State { get; init; }
+    
+    /// <summary>
+    ///     The brewery city.
+    /// </summary>
+    public string? City { get; init; }
+    
+    /// <summary>
+    ///     Minimum foundation year.
+    /// </summary>
+    public double? MinFoundationYear { get; init; } = 0;
+
+    /// <summary>
+    ///     Maximum foundation year.
+    /// </summary>
+    public double? MaxFoundationYear { get; init; } = DateTime.Now.Year;
+}
