9da4dde168e713652368244ce4281125657e4dd0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-26 10:30:59 +0200

    Add GetBreweries endpoint

---
+++ src/Api/Controllers/BreweriesController.cs
@@ -0,0 +1,22 @@
+﻿using Application.Breweries.Dtos;
+using Application.Breweries.Queries.GetBreweries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+public class BreweriesController : ApiControllerBase
+{
+    /// <summary>
+    ///     Gets breweries.
+    /// </summary>
+    /// <returns>An ActionResult of type PaginatedList of BreweryDto</returns>
+    [HttpGet]
+    public async Task<ActionResult<BreweryDto>> GetBeers([FromQuery] GetBreweriesQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        Response.Headers.Add("X-Pagination", result.GetMetadata());
+
+        return Ok(result);
+    }
+}
