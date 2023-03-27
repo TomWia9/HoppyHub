c3db73a45830e6d7b91279dc069820406c10c5cc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-27 16:59:06 +0200

    Add DeleteBrewery command and handler

---
+++ src/Application/Breweries/Commands/DeleteBrewery/DeleteBreweryCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Breweries.Commands.DeleteBrewery;
+
+/// <summary>
+///     DeleteBreweryCommand handler.
+/// </summary>
+public class DeleteBreweryCommandHandler : IRequestHandler<DeleteBreweryCommand>
+{
+    /// <summary>
+    ///     The database context.
+    /// </summary>
+    private readonly IApplicationDbContext _context;
+
+    /// <summary>
+    ///     Initializes DeleteBreweryCommandHandler.
+    /// </summary>
+    /// <param name="context">The database context</param>
+    public DeleteBreweryCommandHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    ///     Handles DeleteBreweryCommand.
+    /// </summary>
+    /// <param name="request">The request</param>
+    /// <param name="cancellationToken">The cancellation token</param>
+    public async Task Handle(DeleteBreweryCommand request, CancellationToken cancellationToken)
+    {
+        var entity =
+            await _context.Breweries.FindAsync(new object?[] { request.Id }, cancellationToken: cancellationToken);
+
+        if (entity == null)
+        {
+            throw new NotFoundException(nameof(Brewery), request.Id);
+        }
+
+        _context.Breweries.Remove(entity);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
