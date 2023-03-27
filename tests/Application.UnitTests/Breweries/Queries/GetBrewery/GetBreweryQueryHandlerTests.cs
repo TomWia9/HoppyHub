637af620bf058711cfab65cdf4cd2239aab40cea
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-27 14:09:49 +0200

    Update GetBreweryQueryHandlerTests

--- tests/Application.UnitTests/Breweries/Queries/GetBrewery/GetBreweryQueryHandlerTests.cs
+++ tests/Application.UnitTests/Breweries/Queries/GetBrewery/GetBreweryQueryHandlerTests.cs
@@ -38,14 +38,24 @@ public class GetBreweryQueryHandlerTests
     }
 
     /// <summary>
-    ///     Tests that Handle method returns BreweryDto when Id is valid.
+    ///     Tests that Handle method returns BreweryDto with AddressDto when Id is valid.
     /// </summary>
     [Fact]
-    public async Task Handle_ShouldReturnBreweryDto_WhenIdIsValid()
+    public async Task Handle_ShouldReturnBreweryDtoWithAddressDto_WhenIdIsValid()
     {
         // Arrange
         var breweryId = Guid.NewGuid();
-        var brewery = new Brewery { Id = breweryId, Name = "Test brewery" };
+        var breweryAddress = new Address
+        {
+            Id = Guid.NewGuid(),
+            City = "Test city",
+            Street = "Test street",
+            Number = "4B",
+            PostCode = "12-345",
+            State = "Test state",
+            Country = "Test country"
+        };
+        var brewery = new Brewery { Id = breweryId, Name = "Test brewery", Address = breweryAddress };
         var breweries = new List<Brewery> { brewery };
         var breweriesDbSetMock = breweries.AsQueryable().BuildMockDbSet();
         _contextMock.Setup(x => x.Breweries).Returns(breweriesDbSetMock.Object);
@@ -59,6 +69,14 @@ public class GetBreweryQueryHandlerTests
         result.Should().NotBeNull();
         result.Id.Should().Be(brewery.Id);
         result.Name.Should().Be(brewery.Name);
+        result.Address.Should().NotBeNull();
+        result.Address?.Id.Should().Be(brewery.Address.Id);
+        result.Address?.City.Should().Be(brewery.Address.City);
+        result.Address?.Street.Should().Be(brewery.Address.Street);
+        result.Address?.Number.Should().Be(brewery.Address.Number);
+        result.Address?.PostCode.Should().Be(brewery.Address.PostCode);
+        result.Address?.State.Should().Be(brewery.Address.State);
+        result.Address?.Country.Should().Be(brewery.Address.Country);
     }
 
     /// <summary>
