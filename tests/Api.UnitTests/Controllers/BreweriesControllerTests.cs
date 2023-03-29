d6a78b457ffa00efb89e5528961a9aa214ef7680
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-03-29 20:53:36 +0200

    Add unit tests for BreweriesController new endpoints

--- tests/Api.UnitTests/Controllers/BreweriesControllerTests.cs
+++ tests/Api.UnitTests/Controllers/BreweriesControllerTests.cs
@@ -1,4 +1,7 @@
 ﻿using Api.Controllers;
+using Application.Breweries.Commands.CreateBrewery;
+using Application.Breweries.Commands.DeleteBrewery;
+using Application.Breweries.Commands.UpdateBrewery;
 using Application.Breweries.Dtos;
 using Application.Breweries.Queries.GetBreweries;
 using Application.Breweries.Queries.GetBrewery;
@@ -60,5 +63,82 @@ public class BreweriesControllerTests : ControllerSetup<BreweriesController>
         // Assert
         response.Result.Should().BeOfType<OkObjectResult>()
             .Which.Value.Should().BeSameAs(expectedResult);
+    }
+
+    /// <summary>
+    ///     Tests that CreateBrewery method returns CreatedAtAction.
+    /// </summary>
+    [Fact]
+    public async Task CreateBrewery_ShouldReturnCreatedAtAction()
+    {
+        // Arrange
+        var command = new CreateBreweryCommand();
+        var expectedResult = new BreweryDto { Id = Guid.NewGuid() };
+        MediatorMock.Setup(m => m.Send(command, CancellationToken.None)).ReturnsAsync(expectedResult);
+
+        // Act
+        var response = await Controller.CreateBrewery(command);
+
+        // Assert
+        response.Result.Should().BeOfType<CreatedAtActionResult>();
+        var createdAtActionResult = (CreatedAtActionResult)response.Result!;
+
+        createdAtActionResult.ActionName.Should().Be("GetBrewery");
+        createdAtActionResult.RouteValues.Should().NotBeNull();
+        createdAtActionResult.RouteValues!["id"].Should().Be(expectedResult.Id);
+        createdAtActionResult.Value.Should().Be(expectedResult);
+    }
+
+    /// <summary>
+    ///     Tests that UpdateBrewery method returns NoContent when Id is valid.
+    /// </summary>
+    [Fact]
+    public async Task UpdateBrewery_ShouldReturnNoContent_WhenIdIsValid()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var command = new UpdateBreweryCommand { Id = id };
+        MediatorMock.Setup(m => m.Send(command, CancellationToken.None)).Returns(Task.CompletedTask);
+
+        // Act
+        var response = await Controller.UpdateBrewery(id, command);
+
+        // Assert
+        response.Should().BeOfType<NoContentResult>();
+    }
+
+    /// <summary>
+    ///     Tests that UpdateBrewery method returns BadRequest when id is invalid.
+    /// </summary>
+    [Fact]
+    public async Task UpdateBrewery_ShouldReturnBadRequest_WhenIdIsInvalid()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        var command = new UpdateBreweryCommand { Id = Guid.NewGuid() };
+
+        // Act
+        var response = await Controller.UpdateBrewery(id, command);
+
+        // Assert
+        response.Should().BeOfType<BadRequestResult>();
+    }
+
+    /// <summary>
+    ///     Tests that DeleteBrewery method returns NoContent.
+    /// </summary>
+    [Fact]
+    public async Task DeleteBrewery_ShouldReturnNoContent()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+        MediatorMock.Setup(m => m.Send(It.IsAny<DeleteBreweryCommand>(), CancellationToken.None))
+            .Returns(Task.CompletedTask);
+
+        // Act
+        var response = await Controller.DeleteBrewery(id);
+
+        // Assert
+        response.Should().BeOfType<NoContentResult>();
     }
 }
