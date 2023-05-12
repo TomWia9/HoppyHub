e1eec8a04eed847e7ecfbf9a56d8d5268540f727
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-12 17:06:43 +0200

    Add PrimaryBeerStyle and SecondaryBeerStyle entities

---
+++ src/Domain/Entities/PrimaryBeerStyle.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+/// <summary>
+///     The primary beer style entity.
+/// </summary>
+public class PrimaryBeerStyle : BaseAuditableEntity
+{
+    /// <summary>
+    ///     The primary beer style name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    ///     The primary beer style description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     The secondary beer styles.
+    /// </summary>
+    public ICollection<SecondaryBeerStyle> SecondaryBeerStyles { get; set; } = new List<SecondaryBeerStyle>();
+}
---
+++ src/Domain/Entities/SecondaryBeerStyle.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities;
+
+/// <summary>
+///     The secondary beer style entity.
+/// </summary>
+public class SecondaryBeerStyle : BaseAuditableEntity
+{
+    /// <summary>
+    ///     The secondary beer style name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    ///     The secondary beer style description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     The country of style origin.
+    /// </summary>
+    public string? CountryOfOrigin { get; set; }
+    
+    /// <summary>
+    ///     The primary beer style id.
+    /// </summary>
+    public Guid PrimaryBeerStyleId { get; set; }
+    
+    /// <summary>
+    ///     The primary beer style.
+    /// </summary>
+    public PrimaryBeerStyle? PrimaryBeerStyle { get; set; }
+}
