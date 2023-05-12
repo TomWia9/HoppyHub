7ddb9955cab19394c9c89f060af797cb9a6210d3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-12 18:16:50 +0200

    Add dtos for new entities

---
+++ src/Application/BeerStyles/Dtos/BeerStyleDto.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Mappings;
+using Application.PrimaryBeerStyles.Dtos;
+using Domain.Entities;
+
+namespace Application.BeerStyles.Dtos;
+
+/// <summary>
+///     The beer style data transfer object.
+/// </summary>
+public record BeerStyleDto : IMapFrom<BeerStyle>
+{
+    /// <summary>
+    ///     The beer style name.
+    /// </summary>
+    public string? Name { get; set; }
+
+    /// <summary>
+    ///     The beer style description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    ///     The country of style origin.
+    /// </summary>
+    public string? CountryOfOrigin { get; set; }
+    
+    /// <summary>
+    ///     The primary beer style.
+    /// </summary>
+    public PrimaryBeerStyleDto? PrimaryBeerStyle { get; set; }
+}
