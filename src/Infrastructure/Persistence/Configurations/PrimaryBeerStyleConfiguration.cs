fe366b0ccd985c364b016fe6c55507427472cc18
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-12 17:12:27 +0200

    Add entities configurations

---
+++ src/Infrastructure/Persistence/Configurations/PrimaryBeerStyleConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+/// <summary>
+///     The primary beer style entity configuration.
+/// </summary>
+public class PrimaryBeerStyleConfiguration : BaseConfiguration<PrimaryBeerStyle>
+{
+    /// <summary>
+    ///     Configures the primary beer style entity.
+    /// </summary>
+    /// <param name="builder">The builder</param>
+    public override void Configure(EntityTypeBuilder<PrimaryBeerStyle> builder)
+    {
+        base.Configure(builder);
+        
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(1000);
+    }
+}
---
+++ src/Infrastructure/Persistence/Configurations/SecondaryBeerStyleConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+/// <summary>
+///     The secondary beer style entity configuration.
+/// </summary>
+public class SecondaryBeerStyleConfiguration : BaseConfiguration<SecondaryBeerStyle>
+{
+    /// <summary>
+    ///     Configures the secondary beer style entity.
+    /// </summary>
+    /// <param name="builder">The builder</param>
+    public override void Configure(EntityTypeBuilder<SecondaryBeerStyle> builder)
+    {
+        base.Configure(builder);
+        
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+        builder.Property(x => x.Description).IsRequired().HasMaxLength(1000);
+        builder.Property(x => x.CountryOfOrigin).IsRequired().HasMaxLength(50);
+    }
+}
