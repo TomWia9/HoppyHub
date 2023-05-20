a0d89de1320d10038c3886ee2dee74e453942d4f
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-20 14:12:53 +0200

    Add GetBeerStylesQuery

---
+++ src/Application/BeerStyles/Queries/GetBeerStyles/GetBeerStylesQuery.cs
@@ -0,0 +1,17 @@
+﻿using Application.BeerStyles.Dtos;
+using Application.Common.Abstractions;
+using Application.Common.Models;
+using MediatR;
+
+namespace Application.BeerStyles.Queries.GetBeerStyles;
+
+/// <summary>
+///     GetBeerStyles query.
+/// </summary>
+public record GetBeerStylesQuery : QueryParameters, IRequest<PaginatedList<BeerStyleDto>>
+{
+    /// <summary>
+    ///     The country of beer style origin.
+    /// </summary>
+    public string? CountryOfOrigin { get; init; }
+}
