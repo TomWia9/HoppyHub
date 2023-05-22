bb4ad99b115a2a01bd7564b1583c2543c94608db
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-22 10:52:48 +0200

    Add GetBeerStylesQueryValidator

---
+++ src/Application/BeerStyles/Queries/GetBeerStyles/GetBeerStylesQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Abstractions;
+using FluentValidation;
+
+namespace Application.BeerStyles.Queries.GetBeerStyles;
+
+/// <summary>
+///     GetBeerStylesQuery validator.
+/// </summary>
+public class GetBeerStylesQueryValidator : QueryValidator<GetBeerStylesQuery>
+{
+    /// <summary>
+    ///     Initializes GetBeerStylesQueryValidator.
+    /// </summary>
+    public GetBeerStylesQueryValidator()
+    {
+        RuleFor(x => x.CountryOfOrigin).MaximumLength(50);
+    }
+}
