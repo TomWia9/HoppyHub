15dc04ff075c816025af01180a433fde4350daa8
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-22 11:24:33 +0200

    Add GetBeerStyle endpoint

--- src/Api/Controllers/BeerStylesController.cs
+++ src/Api/Controllers/BeerStylesController.cs
@@ -1,4 +1,5 @@
 ﻿using Application.BeerStyles.Dtos;
+using Application.BeerStyles.Queries.GetBeerStyle;
 using Application.BeerStyles.Queries.GetBeerStyles;
 using Microsoft.AspNetCore.Mvc;
 
@@ -19,6 +20,19 @@ public class BeerStylesController : ApiControllerBase
         var result = await Mediator.Send(query);
 
         Response.Headers.Add("X-Pagination", result.GetMetadata());
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    ///     Gets beer style by id.
+    /// </summary>
+    /// <param name="id">The beer style id</param>
+    /// <returns>An ActionResult of type BeerStyleDto</returns>
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<BeerStyleDto>> GetBeerStyle(Guid id)
+    {
+        var result = await Mediator.Send(new GetBeerStyleQuery { Id = id });
 
         return Ok(result);
     }
