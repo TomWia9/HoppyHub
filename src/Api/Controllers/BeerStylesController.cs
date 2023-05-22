a0b6d366b48bdf7a879a249fb0bdd06d0dec616f
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-22 11:09:25 +0200

    Add GetBeerStyles endpoint

---
+++ src/Api/Controllers/BeerStylesController.cs
@@ -0,0 +1,25 @@
+﻿using Application.BeerStyles.Dtos;
+using Application.BeerStyles.Queries.GetBeerStyles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+///     The beer styles controller.
+/// </summary>
+public class BeerStylesController : ApiControllerBase
+{
+    /// <summary>
+    ///     Gets beer styles.
+    /// </summary>
+    /// <returns>An ActionResult of type PaginatedList of BeerStyleDto</returns>
+    [HttpGet]
+    public async Task<ActionResult<BeerStyleDto>> GetBeerStyles([FromQuery] GetBeerStylesQuery query)
+    {
+        var result = await Mediator.Send(query);
+
+        Response.Headers.Add("X-Pagination", result.GetMetadata());
+
+        return Ok(result);
+    }
+}
