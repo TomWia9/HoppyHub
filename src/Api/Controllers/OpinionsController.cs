9f5a8006ead42bd3dfa7b27a24f5d902d337a421
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-23 22:20:17 +0200

    Add endpoint and opinions seeder

---
+++ src/Api/Controllers/OpinionsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Opinions.Dtos;
+using Application.Opinions.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+///     The opinions controller.
+/// </summary>
+public class OpinionsController : ApiControllerBase
+{
+    /// <summary>
+    ///     Gets opinion by id.
+    /// </summary>
+    /// <param name="id">The opinion id</param>
+    /// <returns>An ActionResult of type OpinionDto</returns>
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<OpinionDto>> GetOpinion(Guid id)
+    {
+        var result = await Mediator.Send(new GetOpinionQuery { Id = id });
+
+        return Ok(result);
+    }
+}
---
+++ src/Infrastructure/Persistence/Data/Opinions.sql
@@ -0,0 +1,27 @@
+﻿DECLARE @UserId          UNIQUEIDENTIFIER
+DECLARE @OpinionsPerBeer INT
+
+-- Number of opinions generating for beer
+SET @OpinionsPerBeer = 20
+
+-- Select Id for "user@localhost" user
+SELECT @UserId = Id
+FROM AspNetUsers
+WHERE Username = 'user@localhost'
+
+-- Generate @OpinionsPerBeer opinions for each beer
+INSERT INTO Opinions (Id, Rate, Comment, BeerId, Created, CreatedBy, LastModified, LastModifiedBy)
+SELECT NEWID(),
+       FLOOR(RAND(CHECKSUM(NEWID())) * 10) + 1,
+       'Sample comment',
+       Beers.Id,
+       CreatedDate,
+       @UserId,
+       CreatedDate,
+       @UserId
+FROM Beers
+         CROSS JOIN (SELECT TOP (@OpinionsPerBeer) DATEADD(DAY, ABS(CHECKSUM(NEWID())) %
+                                                                (DATEDIFF(DAY, '2020-01-01', '2023-12-31') + 1),
+                                                           '2020-01-01') AS CreatedDate
+                     FROM sys.all_columns) AS Opinions
+ORDER BY Beers.Id
