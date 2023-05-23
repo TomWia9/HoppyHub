d8793e60afd7c5a034be26b64bcae31f11351468
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-23 21:09:58 +0200

    Add OpinionDto

---
+++ src/Application/Opinions/Dtos/OpinionDto.cs
@@ -0,0 +1,50 @@
+﻿using Application.Common.Mappings;
+using Domain.Entities;
+
+namespace Application.Opinions.Dtos;
+
+/// <summary>
+///     The opinion data transfer object.
+/// </summary>
+public record OpinionDto : IMapFrom<Opinion>
+{
+    /// <summary>
+    ///     The opinion id.
+    /// </summary>
+    public Guid Id { get; init; }
+
+    /// <summary>
+    ///     The rate in 1-10 scale.
+    /// </summary>
+    public int Rate { get; init; }
+
+    /// <summary>
+    ///     The comment.
+    /// </summary>
+    public string? Comment { get; init; }
+
+    /// <summary>
+    ///     The beer id.
+    /// </summary>
+    public Guid BeerId { get; init; }
+
+    /// <summary>
+    ///     The user id.
+    /// </summary>
+    public Guid? CreatedBy { get; init; }
+
+    /// <summary>
+    ///     The username.
+    /// </summary>
+    public string? Username { get; init; }
+
+    /// <summary>
+    ///     Date of creation.
+    /// </summary>
+    public DateTime? Created { get; init; }
+
+    /// <summary>
+    ///     Date of modification.
+    /// </summary>
+    public DateTime? Modified { get; init; }
+}
