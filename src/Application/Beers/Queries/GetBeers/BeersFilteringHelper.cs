d11294a02bf666b0c09924e5e3108ddde8839d0b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-23 11:58:58 +0200

    Extend beers filtering by release date

--- src/Application/Beers/Queries/GetBeers/BeersFilteringHelper.cs
+++ src/Application/Beers/Queries/GetBeers/BeersFilteringHelper.cs
@@ -40,8 +40,10 @@ public static class BeersFilteringHelper
         var delegates = new List<Expression<Func<Beer, bool>>>
         {
             x => x.AlcoholByVolume >= request.MinAlcoholByVolume && x.AlcoholByVolume <= request.MaxAlcoholByVolume,
-            x => x.Blg >= request.MinExtract && x.Blg <= request.MaxExtract,
-            x => x.Ibu >= request.MinIbu && x.Ibu <= request.MaxIbu || x.Ibu == null
+            x => (x.Blg >= request.MinExtract && x.Blg <= request.MaxExtract) || x.Blg == null,
+            x => (x.Ibu >= request.MinIbu && x.Ibu <= request.MaxIbu) || x.Ibu == null,
+            x => (x.ReleaseDate >= request.MinReleaseDate && x.ReleaseDate <= request.MaxReleaseDate) ||
+                 x.ReleaseDate == null
         };
 
         if (!string.IsNullOrWhiteSpace(request.Name))
@@ -49,7 +51,7 @@ public static class BeersFilteringHelper
 
         if (request.BreweryId != null)
             delegates.Add(x => x.Brewery != null && x.Brewery.Id == request.BreweryId);
-        
+
         if (request.BeerStyleId != null)
             delegates.Add(x => x.BeerStyle != null && x.BeerStyle.Id == request.BeerStyleId);
 
@@ -63,7 +65,8 @@ public static class BeersFilteringHelper
         Expression<Func<Beer, bool>> searchDelegate =
             x => (x.Name != null && x.Name.ToUpper().Contains(searchQuery)) ||
                  (x.Brewery != null && x.Brewery.Name != null && x.Brewery.Name.ToUpper().Contains(searchQuery)) ||
-                 (x.BeerStyle != null && x.BeerStyle.Name != null && x.BeerStyle.Name.ToUpper().Contains(searchQuery)) ||
+                 (x.BeerStyle != null && x.BeerStyle.Name != null &&
+                  x.BeerStyle.Name.ToUpper().Contains(searchQuery)) ||
                  (x.Description != null && x.Description.ToUpper().Contains(searchQuery));
 
         delegates.Add(searchDelegate);
--- src/Application/Beers/Queries/GetBeers/GetBeersQuery.cs
+++ src/Application/Beers/Queries/GetBeers/GetBeersQuery.cs
@@ -54,11 +54,11 @@ public record GetBeersQuery : QueryParameters, IRequest<PaginatedList<BeerDto>>
     ///     Maximum Ibu.
     /// </summary>
     public int? MaxIbu { get; init; } = 200;
-    
+
     /// <summary>
     ///     Minimum beer release date.
     /// </summary>
-    public DateOnly? MinReleaseDate { get; init; } = new DateOnly(0,1,1);
+    public DateOnly? MinReleaseDate { get; init; } = DateOnly.MinValue;
 
     /// <summary>
     ///     Maximum beer release date.
