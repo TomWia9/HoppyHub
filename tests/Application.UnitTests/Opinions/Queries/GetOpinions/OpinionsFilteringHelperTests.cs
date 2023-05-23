600a39a97ecfb5cf916831204e1b8a66d546d88c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-24 00:02:26 +0200

    Add OpinionsFilteringHelperTests

---
+++ tests/Application.UnitTests/Opinions/Queries/GetOpinions/OpinionsFilteringHelperTests.cs
@@ -0,0 +1,88 @@
+﻿using Application.Opinions.Queries.GetOpinions;
+using Domain.Entities;
+
+namespace Application.UnitTests.Opinions.Queries.GetOpinions;
+
+/// <summary>
+///     Tests for the <see cref="OpinionsFilteringHelper"/> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class OpinionsFilteringHelperTests
+{
+    /// <summary>
+    ///     Tests that GetSortingColumn method returns first column when SortBy is null.
+    /// </summary>
+    [Fact]
+    public void GetSortingColumn_ShouldReturnFirstColumn_WhenSortByIsNull()
+    {
+        // Arrange
+        string? sortBy = null;
+
+        // Act
+        var result = OpinionsFilteringHelper.GetSortingColumn(sortBy);
+
+        // Assert
+        result.Should().Be(OpinionsFilteringHelper.SortingColumns.First().Value);
+    }
+
+    /// <summary>
+    ///     Tests that GetSortingColumn method returns correct column when SortBy is provided.
+    /// </summary>
+    [Fact]
+    public void GetSortingColumn_ShouldReturnCorrectColumn_WhenSortByIsProvided()
+    {
+        // Arrange
+        const string sortBy = nameof(Opinion.Rate);
+
+        // Act
+        var result = OpinionsFilteringHelper.GetSortingColumn(sortBy);
+
+        // Assert
+        result.Should().Be(OpinionsFilteringHelper.SortingColumns[sortBy.ToUpper()]);
+    }
+
+    /// <summary>
+    ///     Tests that GetDelegates method returns delegates.
+    /// </summary>
+    [Fact]
+    public void GetDelegates_ShouldReturnDelegates()
+    {
+        // Arrange
+        var request = new GetOpinionsQuery
+        {
+            MinRate = 5,
+            MaxRate = 10,
+            BeerId = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+            SearchQuery = "test"
+        };
+
+        // Act
+        var result = OpinionsFilteringHelper.GetDelegates(request);
+
+        // Assert
+        result.Should().HaveCount(4, "Min and Max are merged into single delegate");
+    }
+
+    /// <summary>
+    ///     Tests that GetDelegates method returns delegates without SearchQuery.
+    /// </summary>
+    [Fact]
+    public void GetDelegates_ShouldReturnDelegatesWithoutSearchQuery()
+    {
+        // Arrange
+        var request = new GetOpinionsQuery()
+        {
+            MinRate = 5,
+            MaxRate = 10,
+            BeerId = Guid.NewGuid(),
+            UserId = Guid.NewGuid(),
+        };
+
+        // Act
+        var result = OpinionsFilteringHelper.GetDelegates(request);
+
+        // Assert
+        result.Should().HaveCount(3, "Min and Max are merged into single delegate");
+    }
+}
