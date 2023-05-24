20ef4a03cc8c8dde87ae33a2f83cdf650015747e
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-24 13:18:54 +0200

    Add UpdateOpinion command and validator

---
+++ src/Application/Opinions/Commands/UpdateOpinion/UpdateOpinionCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Opinions.Commands.UpdateOpinion;
+
+/// <summary>
+///     UpdateOpinionCommand validator.
+/// </summary>
+public class UpdateOpinionCommandValidator : AbstractValidator<UpdateOpinionCommand>
+{
+    /// <summary>
+    ///     Initializes UpdateOpinionCommandValidator.
+    /// </summary>
+    public UpdateOpinionCommandValidator()
+    {
+        RuleFor(x => x.Rate).NotEmpty().InclusiveBetween(1, 10);
+        RuleFor(x => x.Comment).MaximumLength(1000);
+    }
+}
