3c8e772e95c4134cc510404ce053e6392fe02f0a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-26 09:25:48 +0200

    Add CreateFavorite command, handler, validator and endpoint

---
+++ src/Api/Controllers/FavoritesController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Models;
+using Application.Favorites.Commands.CreateFavorite;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+/// <summary>
+///     The favorites controller.
+/// </summary>
+public class FavoritesController : ApiControllerBase
+{
+    /// <summary>
+    ///     Adds beer to favorites.
+    /// </summary>
+    /// <param name="command">The CreateFavoriteCommand</param>
+    /// <returns>An ActionResult</returns>
+    [Authorize(Policy = Policies.UserAccess)]
+    [HttpPost]
+    public async Task<IActionResult> CreateFavorite([FromBody] CreateFavoriteCommand command)
+    {
+        await Mediator.Send(command);
+
+        return NoContent();
+    }
+}
---
+++ src/Application/Favorites/Commands/CreateFavorite/CreateFavoriteCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Application.Favorites.Commands.CreateFavorite;
+
+/// <summary>
+///     CreateFavorite command.
+/// </summary>
+public record CreateFavoriteCommand : IRequest
+{
+    /// <summary>
+    ///     The favorite beer id.
+    /// </summary>
+    public Guid BeerId { get; set; }
+}
