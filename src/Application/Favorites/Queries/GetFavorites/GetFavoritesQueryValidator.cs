58a04d0d942c6e8357afbac4d150cc11b98765c1
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-05-26 13:54:34 +0200

    Add GetFavoritesQueryValidator

---
+++ src/Application/Favorites/Queries/GetFavorites/GetFavoritesQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common.Abstractions;
+using FluentValidation;
+
+namespace Application.Favorites.Queries.GetFavorites;
+
+/// <summary>
+///     GetFavoritesQuery validator.
+/// </summary>
+public class GetFavoritesQueryValidator : QueryValidator<GetFavoritesQuery>
+{
+    /// <summary>
+    ///     Initializes GetFavoritesQueryValidator.
+    /// </summary>
+    public GetFavoritesQueryValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty();
+    }
+}
