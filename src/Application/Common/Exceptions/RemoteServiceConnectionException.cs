bc571d249f198369c91111ca919b170691778eab
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-06-07 22:16:36 +0200

    Add RemoteServiceConnectionException

---
+++ src/Application/Common/Exceptions/RemoteServiceConnectionException.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Exceptions;
+
+/// <summary>
+///     RemoteServiceConnectionException class.
+/// </summary>
+public class RemoteServiceConnectionException : Exception
+{
+    /// <summary>
+    ///     Initializes RemoteServiceConnectionException with message.
+    /// </summary>
+    /// <param name="message">The message</param>
+    public RemoteServiceConnectionException(string message) : base(message)
+    {
+    }
+}
