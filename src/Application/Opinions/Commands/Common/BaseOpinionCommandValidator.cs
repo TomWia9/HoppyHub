2214293621b6c5bec5017af5755c7ba1f1ee568b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-06-08 16:08:05 +0200

    Add validation for max file length in BaseOpinionCommandValidator

--- src/Application/Opinions/Commands/Common/BaseOpinionCommandValidator.cs
+++ src/Application/Opinions/Commands/Common/BaseOpinionCommandValidator.cs
@@ -15,5 +15,7 @@ public abstract class BaseOpinionCommandValidator<TCommand> : AbstractValidator<
     {
         RuleFor(x => x.Rating).NotEmpty().InclusiveBetween(1, 10);
         RuleFor(x => x.Comment).MaximumLength(1000);
+        RuleFor(x => x.Image).Must(x => x == null || x.Length <= 5 * 1024 * 1024)
+            .WithMessage("The file exceeds the maximum size of 5MB.");
     }
 }
