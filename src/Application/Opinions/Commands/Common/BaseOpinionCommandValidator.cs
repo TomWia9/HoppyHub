d189268d8ffaa47964ba6e3aa78fa8d8d4962460
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-06-08 18:29:29 +0200

    Add file extenstion validation to BaseOpinionCommandValidator

--- src/Application/Opinions/Commands/Common/BaseOpinionCommandValidator.cs
+++ src/Application/Opinions/Commands/Common/BaseOpinionCommandValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Microsoft.AspNetCore.Http;
 
 namespace Application.Opinions.Commands.Common;
 
@@ -15,7 +16,21 @@ public abstract class BaseOpinionCommandValidator<TCommand> : AbstractValidator<
     {
         RuleFor(x => x.Rating).NotEmpty().InclusiveBetween(1, 10);
         RuleFor(x => x.Comment).MaximumLength(1000);
-        RuleFor(x => x.Image).Must(x => x == null || x.Length <= 5 * 1024 * 1024)
+        RuleFor(x => x.Image)
+            .Must(BeAValidFile).When(x => x.Image != null)
+            .WithMessage("Only JPG and PNG files are allowed.")
+            .Must(x => x == null || x.Length <= 5 * 1024 * 1024)
             .WithMessage("The file exceeds the maximum size of 5MB.");
+    }
+
+    private bool BeAValidFile(IFormFile? file)
+    {
+        if (file == null)
+            return true;
+
+        var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" };
+        var fileExtension = Path.GetExtension(file.FileName);
+
+        return allowedExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
     }
 }
