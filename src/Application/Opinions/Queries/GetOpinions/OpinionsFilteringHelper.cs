90f9cd8a1f723b645feffe3e4b6e609e405a26af
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-06-09 09:41:12 +0200

    Add HaveImages to GetOpinionsQuery

--- src/Application/Opinions/Queries/GetOpinions/OpinionsFilteringHelper.cs
+++ src/Application/Opinions/Queries/GetOpinions/OpinionsFilteringHelper.cs
@@ -15,7 +15,7 @@ public class OpinionsFilteringHelper : FilteringHelperBase<Opinion, GetOpinionsQ
     public OpinionsFilteringHelper() : base(SortingColumns)
     {
     }
-    
+
     /// <summary>
     ///     Opinions sorting columns.
     /// </summary>
@@ -23,7 +23,7 @@ public class OpinionsFilteringHelper : FilteringHelperBase<Opinion, GetOpinionsQ
     {
         { nameof(Opinion.LastModified).ToUpper(), x => x.LastModified ?? new DateTime() },
         { nameof(Opinion.Rating).ToUpper(), x => x.Rating },
-        { nameof(Opinion.Comment).ToUpper(), x => x.Comment ?? string.Empty },
+        { nameof(Opinion.Comment).ToUpper(), x => x.Comment ?? string.Empty }
     };
 
     /// <summary>
@@ -34,14 +34,19 @@ public class OpinionsFilteringHelper : FilteringHelperBase<Opinion, GetOpinionsQ
     {
         var delegates = new List<Expression<Func<Opinion, bool>>>
         {
-            x => x.Rating >= request.MinRating && x.Rating <= request.MaxRating,
+            x => x.Rating >= request.MinRating && x.Rating <= request.MaxRating
         };
 
         if (request.BeerId != null)
             delegates.Add(x => x.BeerId == request.BeerId);
-        
+
         if (request.UserId != null)
             delegates.Add(x => x.CreatedBy == request.UserId);
+
+        if (request.HaveImages != null)
+        {
+            delegates.Add(x => !string.IsNullOrEmpty(x.ImageUri) == request.HaveImages);
+        }
 
         if (string.IsNullOrWhiteSpace(request.SearchQuery))
         {
