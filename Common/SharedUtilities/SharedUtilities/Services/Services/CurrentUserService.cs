f358da9dbdd98c8b92c682de7f38594082c0e9ee
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-09-20 10:36:45 +0200

    Add SharedUtilities library

---
+++ Common/SharedUtilities/SharedUtilities/Services/Services/CurrentUserService.cs
@@ -0,0 +1,65 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using SharedUtilities.Interfaces;
+using SharedUtilities.Models;
+
+namespace SharedUtilities.Services.Services;
+
+/// <summary>
+///     Current user service class.
+/// </summary>
+public class CurrentUserService : ICurrentUserService
+{
+    /// <summary>
+    ///     Http context accessor.
+    /// </summary>
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    /// <summary>
+    ///     Initializes CurrentUserService.
+    /// </summary>
+    /// <param name="httpContextAccessor">Http context accessor</param>
+    public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    /// <summary>
+    ///     Current user id.
+    /// </summary>
+    public Guid? UserId => GetCurrentUserId();
+
+    /// <summary>
+    ///     Current user role.
+    /// </summary>
+    public string? UserRole => GetCurrentUserRole();
+
+    /// <summary>
+    ///     Indicates whether current user has admin access.
+    /// </summary>
+    public bool AdministratorAccess => GetCurrentUserRole() == Roles.Administrator;
+
+    /// <summary>
+    ///     Gets current user id.
+    /// </summary>
+    private Guid? GetCurrentUserId()
+    {
+        var userId = _httpContextAccessor.HttpContext?.User.FindFirstValue(ClaimTypes.NameIdentifier);
+        return string.IsNullOrEmpty(userId) ? null : Guid.Parse(userId);
+    }
+
+    /// <summary>
+    ///     Gets current user role.
+    /// </summary>
+    private string? GetCurrentUserRole()
+    {
+        if (_httpContextAccessor.HttpContext is not null)
+        {
+            if (_httpContextAccessor.HttpContext.User.IsInRole(Roles.User)) return Roles.User;
+
+            if (_httpContextAccessor.HttpContext.User.IsInRole(Roles.Administrator)) return Roles.Administrator;
+        }
+
+        return null;
+    }
+}
