2fc8eb344c46dbd69dd52f76bd51e63f29f7a4e4
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-10-20 12:12:42 +0200

    Add SharedEvents project

---
+++ Common/SharedUtilities/SharedEvents/UserCreated.cs
@@ -0,0 +1,27 @@
+﻿namespace SharedEvents;
+
+/// <summary>
+///     The user created event.
+/// </summary>
+public record UserCreated
+{
+    /// <summary>
+    ///     The user id.
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    ///     The email.
+    /// </summary>
+    public string? Email { get; set; }
+
+    /// <summary>
+    ///     The username.
+    /// </summary>
+    public string? Username { get; set; }
+
+    /// <summary>
+    ///     The role.
+    /// </summary>
+    public string? Role { get; set; }
+}
