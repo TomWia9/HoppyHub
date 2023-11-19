edfd7b49d097e9c686e1f74e5925baa498b2cdb9
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-11-19 11:25:46 +0100

    Add BeerUpdated consumer

---
+++ Common/SharedUtilities/SharedEvents/Events/BeerUpdated.cs
@@ -0,0 +1,22 @@
+﻿namespace SharedEvents.Events;
+
+/// <summary>
+///     The beer updated event.
+/// </summary>
+public record BeerUpdated
+{
+    /// <summary>
+    ///     The beer id.
+    /// </summary>
+    public Guid Id { get; set; }
+    
+    /// <summary>
+    ///     The beer name.
+    /// </summary>
+    public string? Name { get; init; }
+
+    /// <summary>
+    ///     The beer brewery name.
+    /// </summary>
+    public string? BreweryName { get; init; }
+}
---
+++ Services/FavoriteManagement/src/Application/Beers/EventConsumers/BeerUpdatedConsumer.cs
@@ -0,0 +1,44 @@
+﻿using Application.Common.Interfaces;
+using MassTransit;
+using SharedEvents.Events;
+
+namespace Application.Beers.EventConsumers;
+
+/// <summary>
+///     BeerUpdated consumer.
+/// </summary>
+public class BeerUpdatedConsumer : IConsumer<BeerUpdated>
+{
+    /// <summary>
+    ///     The database context.
+    /// </summary>
+    private readonly IApplicationDbContext _context;
+
+    /// <summary>
+    ///     Initializes BeerUpdatedConsumer.
+    /// </summary>
+    /// <param name="context">The database context</param>
+    public BeerUpdatedConsumer(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    ///     Consumes BeerUpdated event.
+    /// </summary>
+    /// <param name="context">The consume context</param>
+    public async Task Consume(ConsumeContext<BeerUpdated> context)
+    {
+        var message = context.Message;
+
+        var beer = await _context.Beers.FindAsync(message.Id);
+
+        if (beer is not null)
+        {
+            beer.Name = message.Name;
+            beer.BreweryName = message.BreweryName;
+            
+            await _context.SaveChangesAsync(CancellationToken.None);
+        }
+    }
+}
