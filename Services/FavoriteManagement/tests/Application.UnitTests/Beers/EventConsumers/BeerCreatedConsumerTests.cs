431a16a1d04372e2c3766bc074995e49b7b3056d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-11-19 11:50:08 +0100

    Add unit tests for Beer created, updated, deleted consumers

---
+++ Services/FavoriteManagement/tests/Application.UnitTests/Beers/EventConsumers/BeerCreatedConsumerTests.cs
@@ -0,0 +1,67 @@
+﻿using Application.Beers.EventConsumers;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MassTransit;
+using MockQueryable.Moq;
+using Moq;
+using SharedEvents.Events;
+
+namespace Application.UnitTests.Beers.EventConsumers;
+
+/// <summary>
+///     Tests for the <see cref="BeerCreatedConsumer" /> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class BeerCreatedConsumerTests
+{
+    /// <summary>
+    ///     The consume context mock.
+    /// </summary>
+    private readonly Mock<ConsumeContext<BeerCreated>> _consumeContextMock;
+
+    /// <summary>
+    ///     The application db context mock.
+    /// </summary>
+    private readonly Mock<IApplicationDbContext> _contextMock;
+
+    /// <summary>
+    ///     The BeerCreated consumer.
+    /// </summary>
+    private readonly BeerCreatedConsumer _consumer;
+
+    /// <summary>
+    ///     Setups BeerCreatedConsumerTests.
+    /// </summary>
+    public BeerCreatedConsumerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+        _consumeContextMock = new Mock<ConsumeContext<BeerCreated>>();
+
+        _consumer = new BeerCreatedConsumer(_contextMock.Object);
+    }
+
+    /// <summary>
+    ///     Tests that Consume method adds beer to database when message id is valid.
+    /// </summary>
+    [Fact]
+    public async Task Consume_ShouldAddBeerToDatabase_WhenMessageIsValid()
+    {
+        // Arrange
+        var beerId = Guid.NewGuid();
+        var beers = Enumerable.Empty<Beer>();
+        var beersDbSetMock = beers.AsQueryable().BuildMockDbSet();
+        var message = new BeerCreated
+        {
+            Id = beerId
+        };
+        _consumeContextMock.Setup(x => x.Message).Returns(message);
+        _contextMock.Setup(x => x.Beers).Returns(beersDbSetMock.Object);
+
+        // Act
+        await _consumer.Consume(_consumeContextMock.Object);
+
+        // Assert
+        _contextMock.Verify(x => x.Beers.AddAsync(It.IsAny<Beer>(), It.IsAny<CancellationToken>()), Times.Once);
+        _contextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
---
+++ Services/FavoriteManagement/tests/Application.UnitTests/Beers/EventConsumers/BeerDeletedConsumerTests.cs
@@ -0,0 +1,68 @@
+﻿using Application.Beers.EventConsumers;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MassTransit;
+using Moq;
+using SharedEvents.Events;
+
+namespace Application.UnitTests.Beers.EventConsumers;
+
+/// <summary>
+///     Tests for the <see cref="BeerDeletedConsumer" /> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class BeerDeletedConsumerTests
+{
+    /// <summary>
+    ///     The consume context mock.
+    /// </summary>
+    private readonly Mock<ConsumeContext<BeerDeleted>> _consumeContextMock;
+
+    /// <summary>
+    ///     The application db context mock.
+    /// </summary>
+    private readonly Mock<IApplicationDbContext> _contextMock;
+
+    /// <summary>
+    ///     The BeerDeleted consumer.
+    /// </summary>
+    private readonly BeerDeletedConsumer _consumer;
+
+    /// <summary>
+    ///     Setups BeerDeletedConsumerTests.
+    /// </summary>
+    public BeerDeletedConsumerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+        _consumeContextMock = new Mock<ConsumeContext<BeerDeleted>>();
+
+        _consumer = new BeerDeletedConsumer(_contextMock.Object);
+    }
+
+    /// <summary>
+    ///     Tests that Consume method deleted beer from database when message id is valid.
+    /// </summary>
+    [Fact]
+    public async Task Consume_ShouldDeleteBeerFromDatabase_WhenMessageIsValid()
+    {
+        // Arrange
+        var beerId = Guid.NewGuid();
+        var existingBeer = new Beer
+        {
+            Id = beerId
+        };
+        var message = new BeerDeleted
+        {
+            Id = beerId
+        };
+        _consumeContextMock.Setup(x => x.Message).Returns(message);
+        _contextMock.Setup(x => x.Beers.FindAsync(beerId)).ReturnsAsync(existingBeer);
+
+        // Act
+        await _consumer.Consume(_consumeContextMock.Object);
+
+        // Assert
+        _contextMock.Verify(x => x.Beers.Remove(It.IsAny<Beer>()), Times.Once);
+        _contextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
