0ee3f145759c481fd0defab9ad5e6c3024655199
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-11-25 16:07:55 +0100

    Add FavoritesCountChangedConsumer unit test

---
+++ Services/BeerManagement/tests/Application.UnitTests/Beers/EventConsumers/FavoritesCountChangedConsumerTests.cs
@@ -0,0 +1,70 @@
+﻿using Application.Beers.EventConsumers;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using MassTransit;
+using Moq;
+using SharedEvents.Events;
+
+namespace Application.UnitTests.Beers.EventConsumers;
+
+/// <summary>
+///     Tests for the <see cref="FavoritesCountChangedConsumer" /> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class FavoritesCountChangedConsumerTests
+{
+    /// <summary>
+    ///     The consume context mock.
+    /// </summary>
+    private readonly Mock<ConsumeContext<FavoritesCountChanged>> _consumeContextMock;
+
+    /// <summary>
+    ///     The application db context mock.
+    /// </summary>
+    private readonly Mock<IApplicationDbContext> _contextMock;
+
+    /// <summary>
+    ///     The FavoritesCountChanged consumer.
+    /// </summary>
+    private readonly FavoritesCountChangedConsumer _consumer;
+
+    /// <summary>
+    ///     Setups BeerCreatedConsumerTests.
+    /// </summary>
+    public FavoritesCountChangedConsumerTests()
+    {
+        _contextMock = new Mock<IApplicationDbContext>();
+        _consumeContextMock = new Mock<ConsumeContext<FavoritesCountChanged>>();
+
+        _consumer = new FavoritesCountChangedConsumer(_contextMock.Object);
+    }
+    
+    /// <summary>
+    ///     Tests that Consume method updates beer FavoritesCount message is valid.
+    /// </summary>
+    [Fact]
+    public async Task Consume_ShouldUpdateBeerFavoritesCount_WhenMessageIsValid()
+    {
+        // Arrange
+        var beerId = Guid.NewGuid();
+        var beer = new Beer
+        {
+            Id = beerId,
+            FavoritesCount = 0
+        };
+        var message = new FavoritesCountChanged
+        {
+            BeerId = beerId,
+            FavoritesCount = 1
+        };
+        _consumeContextMock.Setup(x => x.Message).Returns(message);
+        _contextMock.Setup(x => x.Beers.FindAsync(beerId)).ReturnsAsync(beer);
+
+        // Act
+        await _consumer.Consume(_consumeContextMock.Object);
+
+        // Assert
+        _contextMock.Verify(x => x.Beers.FindAsync(beerId), Times.Once);
+        _contextMock.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+    }
+}
