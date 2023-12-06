bde1d831043553f21060a43567beb868ea791a0b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-12-06 11:08:05 +0100

    Add unit tests for new event validators

---
+++ Common/SharedUtilities/SharedUtilities.UnitTests/EventValidators/Beers/BeerOpinionChangedValidatorTests.cs
@@ -0,0 +1,102 @@
+﻿using FluentValidation.TestHelper;
+using SharedEvents.Events;
+using SharedUtilities.EventValidators.Beers;
+using SharedUtilities.Models;
+
+namespace SharedUtilities.UnitTests.EventValidators.Beers;
+
+/// <summary>
+///     Unit tests for the <see cref="BeerOpinionChanged" /> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class BeerOpinionChangedValidatorTests
+{
+    /// <summary>
+    ///     The validator.
+    /// </summary>
+    private readonly BeerOpinionChangedValidator _validator;
+
+    /// <summary>
+    ///     Setups BeerOpinionChangedValidatorTests.
+    /// </summary>
+    public BeerOpinionChangedValidatorTests()
+    {
+        _validator = new BeerOpinionChangedValidator();
+    }
+
+    /// <summary>
+    ///     Tests that validation should not have error for OpinionsCount when OpinionsCount is valid.
+    /// </summary>
+    [Fact]
+    public void BeerOpinionChanged_ShouldNotHaveValidationErrorForOpinionsCount_WhenOpinionsCountIsValid()
+    {
+        // Arrange
+        var beerOpinionChangedEvent = new BeerOpinionChanged
+        {
+            OpinionsCount = 1
+        };
+
+        // Act
+        var result = _validator.TestValidate(beerOpinionChangedEvent);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.OpinionsCount);
+    }
+
+    /// <summary>
+    ///     Tests that validation should have error for OpinionsCount when OpinionsCount exceeds maximum length.
+    /// </summary>
+    [Fact]
+    public void BeerOpinionChanged_ShouldHaveValidationErrorForOpinionsCount_WhenOpinionsCountExceedsMaximumLength()
+    {
+        // Arrange
+        var beerOpinionChangedEvent = new BeerOpinionChanged
+        {
+            OpinionsCount = -1
+        };
+
+        // Act
+        var result = _validator.TestValidate(beerOpinionChangedEvent);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.OpinionsCount);
+    }
+
+    /// <summary>
+    ///     Tests that validation should not have error for NewBeerRating when NewBeerRating is valid.
+    /// </summary>
+    [Fact]
+    public void BeerOpinionChanged_ShouldNotHaveValidationErrorForNewBeerRating_WhenNewBeerRatingIsValid()
+    {
+        // Arrange
+        var beerOpinionChangedEvent = new BeerOpinionChanged
+        {
+            NewBeerRating = 5.55
+        };
+
+        // Act
+        var result = _validator.TestValidate(beerOpinionChangedEvent);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.NewBeerRating);
+    }
+
+    /// <summary>
+    ///     Tests that validation should have error for NewBeerRating when NewBeerRating is empty.
+    /// </summary>
+    [Fact]
+    public void BeerOpinionChanged_ShouldHaveValidationErrorForNewBeerRating_WhenNewBeerRatingIsEmpty()
+    {
+        // Arrange
+        var beerOpinionChangedEvent = new BeerOpinionChanged
+        {
+            NewBeerRating = -1
+        };
+
+        // Act
+        var result = _validator.TestValidate(beerOpinionChangedEvent);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.NewBeerRating);
+    }
+}
---
+++ Common/SharedUtilities/SharedUtilities.UnitTests/EventValidators/Beers/FavoritesCountChangedValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation.TestHelper;
+using SharedEvents.Events;
+using SharedUtilities.EventValidators.Beers;
+
+namespace SharedUtilities.UnitTests.EventValidators.Beers;
+
+/// <summary>
+///     Unit tests for the <see cref="FavoritesCountChanged" /> class.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class FavoritesCountChangedValidatorTests
+{
+    /// <summary>
+    ///     The validator.
+    /// </summary>
+    private readonly FavoritesCountChangedValidator _validator;
+
+    /// <summary>
+    ///     Setups FavoritesCountChangedValidatorTests.
+    /// </summary>
+    public FavoritesCountChangedValidatorTests()
+    {
+        _validator = new FavoritesCountChangedValidator();
+    }
+
+    /// <summary>
+    ///     Tests that validation should not have error for FavoritesCount when FavoritesCount is valid.
+    /// </summary>
+    [Fact]
+    public void FavoritesCountChanged_ShouldNotHaveValidationErrorForFavoritesCount_WhenFavoritesCountIsValid()
+    {
+        // Arrange
+        var favoritesCountChangedEvent = new FavoritesCountChanged
+        {
+            FavoritesCount = 1
+        };
+
+        // Act
+        var result = _validator.TestValidate(favoritesCountChangedEvent);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.FavoritesCount);
+    }
+
+    /// <summary>
+    ///     Tests that validation should have error for FavoritesCount when FavoritesCount exceeds maximum length.
+    /// </summary>
+    [Fact]
+    public void
+        FavoritesCountChanged_ShouldHaveValidationErrorForFavoritesCount_WhenFavoritesCountExceedsMaximumLength()
+    {
+        // Arrange
+        var favoritesCountChangedEvent = new FavoritesCountChanged
+        {
+            FavoritesCount = -1
+        };
+
+        // Act
+        var result = _validator.TestValidate(favoritesCountChangedEvent);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.FavoritesCount);
+    }
+}
