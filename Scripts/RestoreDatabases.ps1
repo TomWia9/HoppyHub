14feb2316405d250de772eaa8a79930a8e540c14
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-12-07 18:49:08 +0100

    Update script

--- Scripts/RestoreDatabases.ps1
+++ Scripts/RestoreDatabases.ps1
@@ -2,58 +2,49 @@ param(
     [string]$Server = "localhost\SQLEXPRESS"
 )
 
-# List of solutions
-$solutions = @(
-    "BeerManagement",
-    "FavoriteManagement",
-    "OpinionManagement",
-    "UserManagement"
-)
-
 # Get the current directory
 $initialDirectory = Convert-Path -Path "..\"
 
-# Loop through each solution
-foreach ($solution in $solutions) {
-    Write-Host "Restoring database for solution: $solution" -ForegroundColor Blue
-    
+function Restore-Database {
+    param (
+        [string]$solution
+    )
+
     # Extract the directory path
     $solutionDirectory = Join-Path -Path (Join-Path -Path $initialDirectory -ChildPath "Services") -ChildPath "$solution"
 
     # Change to the solution directory
     Set-Location -Path $solutionDirectory
-    
+
     # Clean the database (drop and recreate)
     dotnet ef database drop --force --project src/api
     dotnet ef database update --project src/api
 
-    # Return to the initial directory
-    Set-Location -Path $initialDirectory
-
     Write-Host "Success" -ForegroundColor Green
 }
 
-Write-Host "Seeding BeerManagement database" -ForegroundColor DarkBlue
-Write-Host "Seeding Breweries..." -ForegroundColor Blue
+function Initialize-Database {
+    param (
+        [string]$name,
+        [string]$database
+    )
 
-# Execute the SQL script to seed the database
-$scriptPath = Join-Path -Path $initialDirectory -ChildPath "Scripts\SQLScripts\Breweries.sql"
-Invoke-Sqlcmd -InputFile $scriptPath -ServerInstance $Server -Database "BeerManagement"
+    $scriptPath = Join-Path -Path $initialDirectory -ChildPath "Scripts\SQLScripts\$name.sql"
+    Write-Host "Seeding $name..." -ForegroundColor Blue
+    Invoke-Sqlcmd -InputFile $scriptPath -ServerInstance $Server -Database "BeerManagement"
+}
 
-Write-Host "Breweries seeded successfully" -ForegroundColor Green
+function Restore-BeerManagement {
+    $solution = "BeerManagement"
+    Write-Host "Restoring database for $solution" -ForegroundColor DarkMagenta
+    Restore-Database -solution $solution
+    Write-Host "Seeding $solution..." -ForegroundColor DarkMagenta
+    Initialize-Database -name "Breweries" -Database "BeerManagement"
+    Initialize-Database -name "BeerStyles" -Database "BeerManagement"
+    Initialize-Database -name "Beers" -Database "BeerManagement"
+    Write-Host "$solution restored successfully" -ForegroundColor Green
+}
 
-Write-Host "Seeding Beer styles..." -ForegroundColor Blue
+Restore-BeerManagement
 
-# Execute the SQL script to seed the database
-$scriptPath = Join-Path -Path $initialDirectory -ChildPath "Scripts\SQLScripts\BeerStyles.sql"
-Invoke-Sqlcmd -InputFile $scriptPath -ServerInstance $Server -Database "BeerManagement"
-
-Write-Host "Beer styles seeded successfully" -ForegroundColor Green
-
-Write-Host "Seeding Beers..." -ForegroundColor Blue
-
-# Execute the SQL script to seed the database
-$scriptPath = Join-Path -Path $initialDirectory -ChildPath "Scripts\SQLScripts\Beers.sql"
-Invoke-Sqlcmd -InputFile $scriptPath -ServerInstance $Server -Database "BeerManagement"
-
-Write-Host "Beers seeded successfully" -ForegroundColor Green
+Set-Location -Path $initialDirectory
