60e31c0c1a3da1db1340cb40202859d7bde55939
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-12-08 22:36:24 +0100

    Improve RestoreDatabases.ps1 script

--- Scripts/RestoreDatabases.ps1
+++ Scripts/RestoreDatabases.ps1
@@ -1,32 +1,37 @@
+# Set default parameters for the server, username, and password. 
+# If not provided, it assumes a local SQL Server instance.
 param(
     [string]$server = "localhost\SQLEXPRESS",
     [string]$user = "",
     [string]$password = ""
 )
 
-# Get the current directory
+# Get the current directory and navigate back one level.
 $initialDirectory = Convert-Path -Path "..\"
 
+# A function to restore a specified database.
 function Restore-Database
 {
     param (
         [string]$solution
     )
 
-    # Get connection string
+    # Construct a connection string using provided or default parameters.
     $connectionString = "Server=$server;Database=$solution;User=$user;Password=$password;Trusted_Connection=True;TrustServerCertificate=True"
 
-    # Extract the directory path
+    # Create a path for the directory of the specific solution's.
     $solutionDirectory = Join-Path -Path (Join-Path -Path $initialDirectory -ChildPath "Services") -ChildPath "$solution"
 
-    # Change to the solution directory
+    # Change the current directory to the solution's directory.
     Set-Location -Path $solutionDirectory
 
-    # Clean the database (drop and recreate)
-    dotnet ef database update 0 --connection $connectionString --project src/api
-    dotnet ef database update --connection $connectionString --project src/api --no-build
+    # Execute Entity Framework commands to update the database schema.
+    dotnet ef database update 0 --connection $connectionString --project src/api > $null
+    dotnet ef database update --connection $connectionString --project src/api --no-build > $null
+
 }
 
+# A function to initialize a database using SQL scripts.
 function Initialize-Database
 {
     param (
@@ -34,13 +39,17 @@ function Initialize-Database
         [string]$databaseName
     )
 
+    # Execute SQL scripts to initialize the database.
     $scriptPath = Join-Path -Path $initialDirectory -ChildPath "Scripts\SQLScripts\$scriptName.sql"
-    Write-Host "Seeding $scriptName..." -ForegroundColor Blue
+    Write-Host "Seeding $scriptName..." 
     Invoke-Sqlcmd -InputFile $scriptPath -ServerInstance $server -Database $databaseName
+
 }
 
-function Restore-BeerManagement
-{
+# Functions for specific databases to be restored and initialized:
+
+# Restoring and initializing the BeerManagement database.
+function Restore-BeerManagement {
     $solution = "BeerManagement"
     Write-Host "Restoring database for $solution" -ForegroundColor DarkMagenta
     Restore-Database -solution $solution
@@ -50,8 +59,8 @@ function Restore-BeerManagement
     Write-Host "$solution restored successfully" -ForegroundColor Green
 }
 
-function Restore-UserManagement
-{
+# Restoring and initializing the UserManagement database.
+function Restore-UserManagement {
     $solution = "UserManagement"
     Write-Host "Restoring database for $solution" -ForegroundColor DarkMagenta
     Restore-Database -solution $solution
@@ -59,8 +68,8 @@ function Restore-UserManagement
     Write-Host "$solution restored successfully" -ForegroundColor Green
 }
 
-function Restore-OpinionManagement
-{
+# Restoring and initializing the OpinionManagement database.
+function Restore-OpinionManagement {
     $solution = "OpinionManagement"
     Write-Host "Restoring database for $solution" -ForegroundColor DarkMagenta
     Restore-Database -solution $solution
@@ -70,8 +79,8 @@ function Restore-OpinionManagement
     Write-Host "$solution restored successfully" -ForegroundColor Green
 }
 
-function Restore-FavoriteManagement
-{
+# Restoring and initializing the FavoriteManagement database.
+function Restore-FavoriteManagement {
     $solution = "FavoriteManagement"
     Write-Host "Restoring database for $solution" -ForegroundColor DarkMagenta
     Restore-Database -solution $solution
@@ -81,10 +90,13 @@ function Restore-FavoriteManagement
     Write-Host "$solution restored successfully" -ForegroundColor Green
 }
 
-# Restore databases
+# Perform database restoration and initialization for different databases:
 Restore-BeerManagement
 Restore-UserManagement
 Restore-OpinionManagement
 Restore-FavoriteManagement
 
+Write-Host "ALL DATABASES RESTORED SUCCESSFULLY" -ForegroundColor Green
+
+# After restoring all databases, navigate back to the initial script directory.
 Set-Location -Path "$initialDirectory/Scripts"
