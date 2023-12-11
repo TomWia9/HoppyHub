bf91c6712cafef12c56a489e85e14753d8e0cfed
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2023-12-11 20:59:47 +0100

    Update sql scripts

--- Scripts/SQLScripts/FavoriteManagement_Users.sql
+++ Scripts/SQLScripts/FavoriteManagement_Users.sql
@@ -6,5 +6,5 @@ SELECT @UserId = Id
 FROM UserManagement.dbo.AspNetUsers
 WHERE UserName = @UserName
 
-INSERT INTO Users (Id, Username, Role, Created, CreatedBy, LastModified, LastModifiedBy)
-VALUES (@UserId, @UserName, 'User', GETDATE(), @UserId, GETDATE(), @UserId)
+INSERT INTO Users (Id, Username, Role)
+VALUES (@UserId, @UserName, 'User')
