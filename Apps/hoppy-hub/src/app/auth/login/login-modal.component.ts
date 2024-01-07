b10fb670e0237a313fd5f74f250ac4f8bd81a633
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-01-07 19:39:51 +0100

    Add login modal

--- Apps/hoppy-hub/src/app/auth/login/login-modal.component.ts
+++ Apps/hoppy-hub/src/app/auth/login/login-modal.component.ts
@@ -9,11 +9,17 @@ import {
 } from '@angular/core';
 import { LoginModalService } from './login-modal.service';
 import { Subscription } from 'rxjs';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login-modal',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login-modal.component.html',
   styleUrl: './login-modal.component.css'
 })
@@ -21,12 +27,34 @@ export class LoginModalComponent implements OnInit, OnDestroy {
   private loginModalService = inject(LoginModalService);
   @ViewChild('loginModal') myModalRef!: ElementRef;
   loginModalOppenedSubscription!: Subscription;
+  loginForm!: FormGroup;
+  errorMessage = '';
 
   ngOnInit(): void {
     this.loginModalOppenedSubscription =
       this.loginModalService.modalOpened.subscribe(() => {
         this.showModal();
       });
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.email,
+        Validators.required,
+        Validators.maxLength(256)
+      ]),
+      password: new FormControl('', [
+        Validators.minLength(8),
+        Validators.maxLength(64),
+        Validators.required
+      ])
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Sign in data:', this.loginForm.value);
+    } else {
+      console.log('The form is invalid');
+    }
   }
 
   showModal() {
