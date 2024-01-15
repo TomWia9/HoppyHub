c9c15223090a32b8715913cc1c5edb6edede5e55
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-01-15 08:50:23 +0100

    Add username to opinion

--- Apps/hoppy-hub/src/app/users/users.service.ts
+++ Apps/hoppy-hub/src/app/users/users.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
-import { Subject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { User } from './user.model';
 import { environment } from '../../environments/environment';
 
@@ -9,8 +9,6 @@ import { environment } from '../../environments/environment';
 })
 export class UsersService {
   private http: HttpClient = inject(HttpClient);
-
-  opinionsChanged = new Subject<User>();
 
   getUserById(id: string): Observable<User> {
     return this.http.get<User>(
