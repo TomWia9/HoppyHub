59adf69f0d66c663ce13d029fe0d83c59b31a76b
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-16 16:43:15 +0100

    Fetch beers

--- Apps/hoppy-hub/src/app/beers/beers.component.ts
+++ Apps/hoppy-hub/src/app/beers/beers.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { BeersService } from './beers.service';
+import { Beer } from './beer.model';
+import { PagedList } from '../shared/paged-list';
+import { Subscription } from 'rxjs';
+import { BeersParams } from './beers-params';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { ErrorMessageComponent } from '../shared-components/error-message/error-message.component';
 
 @Component({
   selector: 'app-beers',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent, ErrorMessageComponent],
   templateUrl: './beers.component.html',
   styleUrl: './beers.component.css'
 })
-export class BeersComponent {
+export class BeersComponent implements OnInit, OnDestroy {
+  private beersService: BeersService = inject(BeersService);
 
+  beers: PagedList<Beer> | undefined;
+  error = '';
+  loading = true;
+  getBeersSubscription!: Subscription;
+
+  ngOnInit(): void {
+    this.getBeersSubscription = this.beersService
+      .getBeers(new BeersParams(25, 1, 'name', 1))
+      .subscribe({
+        next: (beers: PagedList<Beer>) => {
+          this.loading = true;
+          this.beers = beers;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'An error occurred while loading the beers';
+          this.loading = false;
+        }
+      });
+  }
+  ngOnDestroy(): void {
+    this.getBeersSubscription.unsubscribe();
+  }
 }
