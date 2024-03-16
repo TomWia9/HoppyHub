4e635d4520e7547aa5d4b4d09bb8133b3bea4c72
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-16 15:57:34 +0100

    Add temp beers component

--- Apps/hoppy-hub/src/app/home/home.component.ts
+++ Apps/hoppy-hub/src/app/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { MonthlyDataComponent } from '../monthly-data/monthly-data.component';
 import { RecentOpinionsComponent } from '../recent-opinions/recent-opinions.component';
+import { BeersComponent } from '../beers/beers.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [MonthlyDataComponent, RecentOpinionsComponent]
+  imports: [MonthlyDataComponent, RecentOpinionsComponent, BeersComponent]
 })
 export class HomeComponent {}
