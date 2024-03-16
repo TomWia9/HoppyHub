ce4977c03f611a1a7f8f18eeecee800c55f21f11
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-16 22:43:41 +0100

    Add searching and clearing filters

--- Apps/hoppy-hub/src/app/beers/beers-table/beers-table-filters/beers-table-filters.component.ts
+++ Apps/hoppy-hub/src/app/beers/beers-table/beers-table-filters/beers-table-filters.component.ts
@@ -1,16 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { PaginationComponent } from '../../../shared-components/pagination/pagination.component';
 import { Pagination } from '../../../shared/pagination';
 import { BeersParams } from '../../beers-params';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BeersService } from '../../beers.service';
 
 @Component({
   selector: 'app-beers-table-filters',
   standalone: true,
-  imports: [PaginationComponent],
+  imports: [PaginationComponent, ReactiveFormsModule],
   templateUrl: './beers-table-filters.component.html',
   styleUrl: './beers-table-filters.component.css'
 })
-export class BeersTableFiltersComponent {
+export class BeersTableFiltersComponent implements OnInit {
   @Input({ required: true }) params!: BeersParams;
   @Input({ required: true }) paginationData!: Pagination;
+
+  private beersService: BeersService = inject(BeersService);
+
+  searchForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.searchForm = new FormGroup({
+      search: new FormControl('')
+    });
+  }
+
+  onSearch() {
+    if (this.searchForm.value.search) {
+      this.params.searchQuery = this.searchForm.value.search;
+      this.beersService.paramsChanged.next(this.params);
+    }
+  }
+
+  onFiltersClear() {
+    this.searchForm.reset();
+    this.params = new BeersParams(25, 1, 'ReleaseDate', 1);
+    this.beersService.paramsChanged.next(this.params);
+  }
 }
