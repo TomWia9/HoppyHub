d4778fc11819944ac2bbfbbd5b8b46a1d5295b5c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-20 23:00:09 +0100

    Add container, upload blob and save blob uri to key vault

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -2,6 +2,7 @@ locals {
   resource_group_name  = "HoppyHub1"
   key_vault_name       = "HoppyHub-KeyVault1"
   storage_account_name = "hoppyhub1"
+  blob_container_name = "hoppyhub-container"
 }
 
 #Resource Group
@@ -15,7 +16,6 @@ resource "azurerm_resource_group" "rg" {
 
 #Key Vault
 data "azurerm_client_config" "current" {}
-
 resource "azurerm_key_vault" "key_vault" {
   name                        = local.key_vault_name
   location                    = azurerm_resource_group.rg.location
@@ -36,7 +36,7 @@ resource "azurerm_key_vault" "key_vault" {
   }
 }
 
-#Storage account
+#Storage account, container, blob
 resource "azurerm_storage_account" "storage_account" {
   name                     = local.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
@@ -44,22 +44,30 @@ resource "azurerm_storage_account" "storage_account" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
-
-#Upload image if possible
+resource "azurerm_storage_container" "blob_container" {
+  name                  = local.blob_container_name
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "blob"
+}
+resource "azurerm_storage_blob" "temp_beer_image" {
+  name                   = "Beers/temp.jpg"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.blob_container.name
+  type                   = "Block"
+  source                 = "./assets/temp.jpg"
+}
 
 #Key Vault secrets
+resource "azurerm_key_vault_secret" "kv_secret_temp_beer_image_url" {
+  name         = "TempBeerImageUri"
+  value        = azurerm_storage_blob.temp_beer_image.url
+  key_vault_id = azurerm_key_vault.key_vault.id
+}
 resource "azurerm_key_vault_secret" "kv_secret_jwt" {
   name         = "JwtSettings--Secret"
   value        = "fsaf2faf2j83f2389fj2qa9sj392jwfqa92fjqwi21rfqwa"
   key_vault_id = azurerm_key_vault.key_vault.id
 }
-
-resource "azurerm_key_vault_secret" "kv_secret_temp_beer_image_uri" {
-  name         = "TempBeerImageUri"
-  value        = ""
-  key_vault_id = azurerm_key_vault.key_vault.id
-}
-
 resource "azurerm_key_vault_secret" "kv_secret_blob_connection_string" {
   name         = "BlobContainerSettings--BlobConnectionString"
   value        = azurerm_storage_account.storage_account.primary_connection_string
