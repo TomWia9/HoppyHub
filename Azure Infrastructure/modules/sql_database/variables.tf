39781df101fa75bcf6321eaab3c83d8b7b89c81d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-23 12:53:10 +0100

    Format variables in sql_database module

--- Azure Infrastructure/modules/sql_database/variables.tf
+++ Azure Infrastructure/modules/sql_database/variables.tf
@@ -1,34 +1,34 @@
 variable "db_name" {
-  type = string
+  type        = string
   description = "Database name"
 }
 
 variable "sql_server_id" {
-  type = string
+  type        = string
   description = "SQL Server id"
 }
 
 variable "sql_server_full_name" {
-    type = string
-    description = "SQL Server full name"
+  type        = string
+  description = "SQL Server full name"
 }
 
 variable "sql_server_user" {
-    type = string
-    description = "SQL Server user"
+  type        = string
+  description = "SQL Server user"
 }
 
 variable "sql_server_password" {
-    type = string
-    description = "SQL Server password"
+  type        = string
+  description = "SQL Server password"
 }
 
 variable "connection_string_secret_name" {
-    type = string
+  type        = string
   description = "Connection string secret name"
 }
 
 variable "key_vault_id" {
-    type = string
+  type        = string
   description = "Key vault id"
 }
