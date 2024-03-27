2725ebf17631eb8c18bc53fe73fd9c2c9d0f9ac8
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-27 21:14:27 +0100

    Add BreweryDeletedConsumer in FavoriteManagement

---
+++ Services/FavoriteManagement/src/Application/Breweries/EventConsumers/BreweryDeletedConsumer.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Interfaces;
+using MassTransit;
+using SharedEvents.Events;
+
+namespace Application.Breweries.EventConsumers;
+
+/// <summary>
+///     BreweryDeleted consumer.
+/// </summary>
+public class BreweryDeletedConsumer : IConsumer<BreweryDeleted>
+{
+    /// <summary>
+    ///     The database context.
+    /// </summary>
+    private readonly IApplicationDbContext _context;
+
+    /// <summary>
+    ///     Initializes BreweryDeletedConsumer.
+    /// </summary>
+    /// <param name="context">The database context</param>
+    public BreweryDeletedConsumer(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /// <summary>
+    ///     Consumes BreweryDeleted event.
+    /// </summary>
+    /// <param name="context">The consume context</param>
+    public async Task Consume(ConsumeContext<BreweryDeleted> context)
+    {
+        var message = context.Message;
+        var breweryBeers = _context.Beers.Where(x => x.BreweryId == message.Id);
+
+        _context.Beers.RemoveRange(breweryBeers);
+        await _context.SaveChangesAsync(CancellationToken.None);
+    }
+}
