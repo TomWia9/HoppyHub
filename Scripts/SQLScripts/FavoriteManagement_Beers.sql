628a716fac5d77b0d0b39a36577981709973ff0d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-03-27 17:02:52 +0100

    Update scripts

--- Scripts/SQLScripts/FavoriteManagement_Beers.sql
+++ Scripts/SQLScripts/FavoriteManagement_Beers.sql
@@ -1,8 +1,6 @@
 INSERT INTO Beers (Id, Name, BreweryName, BreweryId)
 SELECT Beers.Id,
        Beers.Name,
-       Breweries.Name,
        Breweries.Id
 FROM BeerManagement.dbo.Beers
-         INNER JOIN BeerManagement.dbo.Breweries ON Beers.BreweryId = Breweries.Id
 ORDER BY Beers.Id
