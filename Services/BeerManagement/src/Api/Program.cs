e48ab095bab2ab75c4c974d2cc93772efc41d6b3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-04-02 21:52:03 +0200

    revert api changes

--- Services/BeerManagement/src/Api/Program.cs
+++ Services/BeerManagement/src/Api/Program.cs
@@ -27,6 +27,7 @@ if (app.Environment.IsDevelopment())
         c.SwaggerEndpoint("swagger/BeerManagementSpecification/swagger.json", "HoppyHub - Beer Management");
         c.RoutePrefix = string.Empty;
     });
+    app.UseCors("AngularApp");
 }
 else
 {
@@ -35,7 +36,6 @@ else
         new DefaultAzureCredential());
 }
 
-app.UseCors("AngularApp");
 app.UseSerilogRequestLogging();
 app.UseAuthentication();
 app.UseAuthorization();
