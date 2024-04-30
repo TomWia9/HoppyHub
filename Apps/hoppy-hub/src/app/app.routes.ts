c02d6dcba63c254f980200bd2790bb6bfc7ccfcd
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-04-30 21:35:22 +0200

    Add breweries route and component

--- Apps/hoppy-hub/src/app/app.routes.ts
+++ Apps/hoppy-hub/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { BreweriesComponent } from './breweries/breweries.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'breweries',
+    component: BreweriesComponent
   },
   { path: 'not-found', component: NotFoundComponent },
   { path: '**', redirectTo: '/not-found' }
---
+++ Apps/hoppy-hub/src/app/breweries/breweries.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breweries',
+  standalone: true,
+  imports: [],
+  templateUrl: './breweries.component.html'
+})
+export class BreweriesComponent {}
