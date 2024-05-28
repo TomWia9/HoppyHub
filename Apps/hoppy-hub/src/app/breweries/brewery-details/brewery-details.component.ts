84b2728b219552c9ef3efd569de44fba45e59080
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-05-28 19:27:20 +0200

    Get brewery beers, add pagination

--- Apps/hoppy-hub/src/app/breweries/brewery-details/brewery-details.component.ts
+++ Apps/hoppy-hub/src/app/breweries/brewery-details/brewery-details.component.ts
@@ -5,12 +5,18 @@ import { ActivatedRoute } from '@angular/router';
 import { Subscription, map } from 'rxjs';
 import { BreweriesService } from '../breweries.service';
 import { Brewery } from '../brewery.model';
+import { BeersService } from '../../beers/beers.service';
+import { Beer } from '../../beers/beer.model';
+import { PagedList } from '../../shared/paged-list';
+import { BeersParams } from '../../beers/beers-params';
+import { Pagination } from '../../shared/pagination';
+import { PaginationComponent } from '../../shared-components/pagination/pagination.component';
 
 @Component({
   selector: 'app-brewery-details',
   standalone: true,
   templateUrl: './brewery-details.component.html',
-  imports: [LoadingSpinnerComponent, ErrorMessageComponent]
+  imports: [LoadingSpinnerComponent, ErrorMessageComponent, PaginationComponent]
 })
 export class BreweryDetailsComponent implements OnInit, OnDestroy {
   brewery!: Brewery;
@@ -18,14 +24,24 @@ export class BreweryDetailsComponent implements OnInit, OnDestroy {
   loading = true;
   routeSubscription!: Subscription;
   brewerySubscription!: Subscription;
+  beersSubscription!: Subscription;
+  beersParamsSubscription!: Subscription;
+  beersParams = new BeersParams(3, 1, 'ReleaseDate', 1);
+  beers: PagedList<Beer> | undefined;
 
   private route: ActivatedRoute = inject(ActivatedRoute);
   private breweriesService: BreweriesService = inject(BreweriesService);
+  private beersService: BeersService = inject(BeersService);
 
   ngOnInit(): void {
     this.routeSubscription = this.route.paramMap
       .pipe(map(params => params.get('id')))
       .subscribe(breweryId => {
+        this.unsubscribeAll();
+        this.beersParams.breweryId = breweryId as string;
+        this.beersParams.pageNumber = 1;
+        this.beersService.paramsChanged.next(this.beersParams);
+
         this.brewerySubscription = this.breweriesService
           .getBreweryById(breweryId as string)
           .subscribe({
@@ -40,15 +56,69 @@ export class BreweryDetailsComponent implements OnInit, OnDestroy {
               this.loading = false;
             }
           });
+
+        this.beersParamsSubscription =
+          this.beersService.paramsChanged.subscribe((params: BeersParams) => {
+            this.beersParams = params;
+            this.getBeers();
+          });
       });
   }
 
-  ngOnDestroy(): void {
-    if (this.routeSubscription) {
-      this.routeSubscription.unsubscribe();
+  private getBeers(): void {
+    this.beersSubscription = this.beersService
+      .getBeers(this.beersParams)
+      .subscribe({
+        next: (beers: PagedList<Beer>) => {
+          this.loading = true;
+          this.beers = beers;
+          this.error = '';
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'An error occurred while loading the beers';
+          this.loading = false;
+        }
+      });
+  }
+
+  getPaginationData(): Pagination {
+    if (this.beers) {
+      return {
+        CurrentPage: this.beers.CurrentPage,
+        HasNext: this.beers.HasNext,
+        HasPrevious: this.beers.HasPrevious,
+        TotalPages: this.beers.TotalPages,
+        TotalCount: this.beers.TotalCount
+      };
     }
+
+    return {
+      CurrentPage: 0,
+      HasNext: false,
+      HasPrevious: false,
+      TotalPages: 0,
+      TotalCount: 0
+    };
+  }
+
+  private unsubscribeAll(): void {
     if (this.brewerySubscription) {
       this.brewerySubscription.unsubscribe();
+    }
+    if (this.beersParamsSubscription) {
+      this.beersParamsSubscription.unsubscribe();
+    }
+    if (this.beersSubscription) {
+      this.beersSubscription.unsubscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribeAll();
+
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
     }
   }
 }
