7c3624f782360624eeca1294b7b3c23baef2a9b7
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-08-06 18:09:20 +0200

    Add beers route and beers table

---
+++ Apps/hoppy-hub/src/app/beers/beers.component.ts
@@ -0,0 +1,58 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterOutlet
+} from '@angular/router';
+import { BeersTableComponent } from './beers-table/beers-table.component';
+import { BeerDetailsComponent } from './beer-details/beer-details.component';
+import { BeersFiltersModalComponent } from './beers-table/beers-table-filters/beers-filters-modal/beers-filters-modal.component';
+import { Subscription, filter } from 'rxjs';
+
+@Component({
+  selector: 'app-beers',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    BeersTableComponent,
+    BeerDetailsComponent,
+    BeersFiltersModalComponent
+  ],
+  templateUrl: './beers.component.html'
+})
+export class BeersComponent implements OnInit, OnDestroy {
+  beerSelected: boolean = false;
+  routeSubscription!: Subscription;
+
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.checkRoute();
+
+    this.routeSubscription = this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.checkRoute();
+      });
+  }
+
+  private checkRoute(): void {
+    const childRoute = this.route.firstChild;
+
+    if (childRoute) {
+      childRoute.paramMap.subscribe(paramMap => {
+        this.beerSelected = paramMap.has('id');
+      });
+    } else {
+      this.beerSelected = false;
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+}
--- Apps/hoppy-hub/src/app/home/home.component.ts
+++ Apps/hoppy-hub/src/app/home/home.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { MonthlyDataComponent } from '../monthly-data/monthly-data.component';
 import { RecentOpinionsComponent } from '../recent-opinions/recent-opinions.component';
-import { BeersTableComponent } from '../beers/beers-table/beers-table.component';
-import { BeersFiltersModalComponent } from '../beers/beers-table/beers-table-filters/beers-filters-modal/beers-filters-modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [
-    MonthlyDataComponent,
-    RecentOpinionsComponent,
-    BeersTableComponent,
-    BeersFiltersModalComponent
-  ]
+  imports: [MonthlyDataComponent, RecentOpinionsComponent]
 })
 export class HomeComponent {}
