26aeaffa682adaae38787cc02697f994d3a70b1c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-08-07 12:08:04 +0200

    Add beer details

--- Apps/hoppy-hub/src/app/beers/beer-details/beer-details.component.ts
+++ Apps/hoppy-hub/src/app/beers/beer-details/beer-details.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription, map } from 'rxjs';
+import { Beer } from '../beer.model';
+import { BeersService } from '../beers.service';
+import { ErrorMessageComponent } from '../../shared-components/error-message/error-message.component';
+import { LoadingSpinnerComponent } from '../../shared-components/loading-spinner/loading-spinner.component';
 
 @Component({
   selector: 'app-beer-details',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent, ErrorMessageComponent],
   templateUrl: './beer-details.component.html'
 })
-export class BeerDetailsComponent {}
+export class BeerDetailsComponent implements OnInit, OnDestroy {
+  beer!: Beer;
+  error = '';
+  loading = true;
+  routeSubscription!: Subscription;
+  beerSubscription!: Subscription;
+
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private beersService: BeersService = inject(BeersService);
+
+  ngOnInit(): void {
+    this.routeSubscription = this.route.paramMap
+      .pipe(map(params => params.get('id')))
+      .subscribe(beerId => {
+        this.beerSubscription = this.beersService
+          .getBeerById(beerId as string)
+          .subscribe({
+            next: (beer: Beer) => {
+              this.loading = true;
+              this.beer = beer;
+              this.error = '';
+              this.loading = false;
+            },
+            error: () => {
+              this.error = 'An error occurred while loading the beer';
+              this.loading = false;
+            }
+          });
+      });
+  }
+
+  ngOnDestroy(): void {
+    if (this.beerSubscription) {
+      this.beerSubscription.unsubscribe();
+    }
+
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+}
