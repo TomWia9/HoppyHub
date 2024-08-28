d7d307bc9e8d2302bce0bc5577908a181ae5e6de
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-08-28 12:51:43 +0200

    Add recent-beers component

--- Apps/hoppy-hub/src/app/home/home.component.ts
+++ Apps/hoppy-hub/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MonthlyDataComponent } from './monthly-data/monthly-data.component';
 import { RecentOpinionsComponent } from './recent-opinions/recent-opinions.component';
+import { RecentBeersComponent } from './recent-beers/recent-beers.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  imports: [MonthlyDataComponent, RecentOpinionsComponent]
+  imports: [MonthlyDataComponent, RecentOpinionsComponent, RecentBeersComponent]
 })
 export class HomeComponent {}
