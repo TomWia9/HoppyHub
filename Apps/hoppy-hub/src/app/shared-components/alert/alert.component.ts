0e751c3a956acbb18217424fedbcae349d88fba7
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-09-13 09:54:51 +0200

    Features/alert improvements (#110)
    * Add timeout for alert
    
    * Set proper alert icon

--- Apps/hoppy-hub/src/app/shared-components/alert/alert.component.ts
+++ Apps/hoppy-hub/src/app/shared-components/alert/alert.component.ts
@@ -12,8 +12,10 @@ import { Alert } from './alert.model';
 })
 export class AlertComponent implements OnInit, OnDestroy {
   private alertService: AlertService = inject(AlertService);
+
   message: string = '';
   alertClass: string = '';
+  alertType: string = '';
   showAlert: boolean = false;
   alertOpenedSubscription!: Subscription;
 
@@ -28,23 +30,33 @@ export class AlertComponent implements OnInit, OnDestroy {
     switch (alert.alertType) {
       case AlertType.Success:
         this.alertClass = 'alert-success';
+        this.alertType = 'success';
         break;
       case AlertType.Error:
         this.alertClass = 'alert-error';
+        this.alertType = 'error';
         break;
       case AlertType.Warning:
         this.alertClass = 'alert-warning';
+        this.alertType = 'warning';
         break;
       case AlertType.Info:
         this.alertClass = 'alert-info';
+        this.alertType = 'info';
         break;
       default:
         this.alertClass = '';
+        this.alertType = '';
         break;
     }
 
     this.message = alert.message;
     this.showAlert = true;
+    if (alert.alertType != AlertType.Error) {
+      setTimeout(() => {
+        this.onAlertClose();
+      }, 3500);
+    }
   }
 
   onAlertClose() {
