0b7f297b71ce091a72054b67c1da93b63bcd55e0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-10-30 09:34:40 +0100

    Move beer opinions list to separated component

--- Apps/hoppy-hub/src/app/beers/beer-details/beer-opinions/beer-opinions.component.ts
+++ Apps/hoppy-hub/src/app/beers/beer-details/beer-opinions/beer-opinions.component.ts
@@ -1,21 +1,16 @@
 import {
   Component,
-  ElementRef,
   EventEmitter,
   inject,
   Input,
-  OnChanges,
   OnDestroy,
-  OnInit,
   Output,
-  ViewChild
+  OnChanges
 } from '@angular/core';
 import { OpinionsService } from '../../../opinions/opinions.service';
 import { OpinionsParams } from '../../../opinions/opinions-params';
 import { Opinion } from '../../../opinions/opinion.model';
 import { map, Subscription, take, tap } from 'rxjs';
-import { PagedList } from '../../../shared/paged-list';
-import { Pagination } from '../../../shared/pagination';
 import { Beer } from '../../beer.model';
 import { OpinionComponent } from '../../../opinions/opinion/opinion.component';
 import { FormsModule } from '@angular/forms';
@@ -25,7 +20,7 @@ import { UpsertOpinionModalComponent } from '../../../opinions/upsert-opinion-mo
 import { AuthUser } from '../../../auth/auth-user.model';
 import { LoadingSpinnerComponent } from '../../../shared-components/loading-spinner/loading-spinner.component';
 import { DeleteOpinionModalComponent } from '../../../opinions/delete-opinion-modal/delete-opinion-modal.component';
-import { DataHelper } from '../../../shared/data-helper';
+import { BeerOpinionsListComponent } from './beer-opinions-list/beer-opinions-list.component';
 
 @Component({
   selector: 'app-beer-opinions',
@@ -36,106 +31,31 @@ import { DataHelper } from '../../../shared/data-helper';
     PaginationComponent,
     UpsertOpinionModalComponent,
     LoadingSpinnerComponent,
-    DeleteOpinionModalComponent
+    DeleteOpinionModalComponent,
+    BeerOpinionsListComponent
   ],
   templateUrl: './beer-opinions.component.html'
 })
-export class BeerOpinionsComponent
-  extends DataHelper
-  implements OnInit, OnChanges, OnDestroy
-{
+export class BeerOpinionsComponent implements OnDestroy, OnChanges {
   @Input({ required: true }) beer!: Beer;
   @Input({ required: true }) user!: AuthUser | null;
   @Output() opinionsCountChanged = new EventEmitter<number>();
-  @ViewChild('opinionsSection') opinionsSection!: ElementRef;
 
   private opinionsService: OpinionsService = inject(OpinionsService);
   private modalService: ModalService = inject(ModalService);
 
-  sortOptions = OpinionsParams.sortOptions;
-
-  opinionsParamsSubscription!: Subscription;
-  getOpinionsSubscription!: Subscription;
-  userSubscription!: Subscription;
   getUserOpinionsSubscription!: Subscription;
-  selectedSortOptionIndex: number = 0;
-  opinionsParams = new OpinionsParams({
-    pageSize: 10,
-    pageNumber: 1,
-    sortBy: 'created',
-    sortDirection: 1
-  });
-  opinions: PagedList<Opinion> | undefined;
-  paginationData!: Pagination;
-  error = '';
-  opinionsLoading = true;
   existingOpinionloading = true;
-  showOpinions = false;
   existingOpinion: Opinion | null = null;
 
-  ngOnInit(): void {
-    this.opinionsParamsSubscription =
-      this.opinionsService.paramsChanged.subscribe((params: OpinionsParams) => {
-        this.opinionsParams = params;
-        this.opinionsParams.beerId = this.beer.id;
-        this.getOpinions();
-      });
-  }
-
   ngOnChanges(): void {
-    this.refreshOpinions(0);
-  }
-
-  refreshOpinions(opinionsCountChange: number): void {
-    this.opinionsParams.beerId = this.beer.id;
-    this.opinionsService.paramsChanged.next(this.opinionsParams);
-    this.existingOpinion = null;
-    this.opinionsCountChanged.emit(opinionsCountChange);
     this.checkIfUserAlreadyAddedOpinion();
   }
 
-  onSort(): void {
-    this.opinionsParams.pageNumber = 1;
-    this.opinionsParams.sortBy =
-      this.sortOptions[this.selectedSortOptionIndex].value;
-    this.opinionsParams.sortDirection =
-      this.sortOptions[this.selectedSortOptionIndex].direction;
-    this.opinionsService.paramsChanged.next(this.opinionsParams);
-  }
-
-  onFiltersClear(): void {
-    this.selectedSortOptionIndex = 0;
-    this.opinionsParams = new OpinionsParams({
-      pageSize: 10,
-      pageNumber: 1,
-      sortBy: 'created',
-      sortDirection: 1
-    });
-
-    if (
-      JSON.stringify(this.opinionsService.paramsChanged.value) !=
-      JSON.stringify(this.opinionsParams)
-    ) {
-      this.opinionsService.paramsChanged.next(this.opinionsParams);
-    }
-  }
-
-  toggleOpinions(): void {
-    if (this.showOpinions) {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-    this.showOpinions = !this.showOpinions;
-  }
-
-  scrollToTop(): void {
-    const elementPosition =
-      this.opinionsSection.nativeElement.getBoundingClientRect().top +
-      window.scrollY;
-
-    window.scrollTo({
-      top: elementPosition,
-      behavior: 'smooth'
-    });
+  refreshOpinionsCount(opinionsCountChange: number): void {
+    this.existingOpinion = null;
+    this.opinionsCountChanged.emit(opinionsCountChange);
+    this.checkIfUserAlreadyAddedOpinion();
   }
 
   onUpsertOpinionModalOpen(): void {
@@ -179,39 +99,7 @@ export class BeerOpinionsComponent
     }
   }
 
-  private getOpinions(): void {
-    this.getOpinionsSubscription = this.opinionsService
-      .getOpinions(this.opinionsParams)
-      .subscribe({
-        next: (opinions: PagedList<Opinion>) => {
-          this.opinionsLoading = true;
-          this.opinions = opinions;
-          this.paginationData = this.getPaginationData(opinions);
-          this.error = '';
-          this.opinionsLoading = false;
-        },
-        error: error => {
-          this.error = 'An error occurred while loading the opinions';
-
-          if (error.error && error.error.errors) {
-            const errorMessage = this.getValidationErrorMessage(
-              error.error.errors
-            );
-            this.error += errorMessage;
-          }
-
-          this.opinionsLoading = false;
-        }
-      });
-  }
-
   ngOnDestroy(): void {
-    if (this.getOpinionsSubscription) {
-      this.getOpinionsSubscription.unsubscribe();
-    }
-    if (this.opinionsParamsSubscription) {
-      this.opinionsParamsSubscription.unsubscribe();
-    }
     if (this.getUserOpinionsSubscription) {
       this.getUserOpinionsSubscription.unsubscribe();
     }
