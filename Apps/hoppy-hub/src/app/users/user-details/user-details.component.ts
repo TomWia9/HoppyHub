814a3067ac8c6967e6d4170b4305217f7a8a5c3c
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-11-17 20:06:56 +0100

    Add user info template

--- Apps/hoppy-hub/src/app/users/user-details/user-details.component.ts
+++ Apps/hoppy-hub/src/app/users/user-details/user-details.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { UserInfoComponent } from './user-info/user-info.component';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [UserInfoComponent],
   templateUrl: './user-details.component.html'
 })
 export class UserDetailsComponent {}
---
+++ Apps/hoppy-hub/src/app/users/user-details/user-info/user-info.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-info.component.html'
+})
+export class UserInfoComponent {}
