7e96a228862b4af9cf345d66671f315f9533d5a0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-11-18 18:11:48 +0100

    Get user beers rated count and favorites count

--- Apps/hoppy-hub/src/app/users/user-details/user-info/user-info.component.ts
+++ Apps/hoppy-hub/src/app/users/user-details/user-info/user-info.component.ts
@@ -1,9 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { map, Subscription } from 'rxjs';
+import { FavoritesService } from '../../../favorites/favorites.service';
+import { FavoritesParams } from '../../../favorites/favorites-params';
+import { PagedList } from '../../../shared/paged-list';
+import { LoadingSpinnerComponent } from '../../../shared-components/loading-spinner/loading-spinner.component';
+import { ErrorMessageComponent } from '../../../shared-components/error-message/error-message.component';
+import { Beer } from '../../../beers/beer.model';
+import { OpinionsService } from '../../../opinions/opinions.service';
+import { OpinionsParams } from '../../../opinions/opinions-params';
+import { Opinion } from '../../../opinions/opinion.model';
+import { faBeerMugEmpty } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [],
+  imports: [LoadingSpinnerComponent, ErrorMessageComponent, FontAwesomeModule],
   templateUrl: './user-info.component.html'
 })
-export class UserInfoComponent {}
+export class UserInfoComponent implements OnInit, OnDestroy {
+  routeSubscription!: Subscription;
+  favoritesSubscription!: Subscription;
+  opinionsSubscription!: Subscription;
+  userFavoritesCount: number = 0;
+  userBeersRatedCount: number = 0;
+  error = '';
+  loading = true;
+  faBeerMugEmpty = faBeerMugEmpty;
+  faHeart = faHeart;
+
+  private route: ActivatedRoute = inject(ActivatedRoute);
+  private favoritesService: FavoritesService = inject(FavoritesService);
+  private opinionsService: OpinionsService = inject(OpinionsService);
+
+  ngOnInit(): void {
+    this.routeSubscription = this.route.paramMap
+      .pipe(map(params => params.get('id')))
+      .subscribe(userId => {
+        this.favoritesSubscription = this.favoritesService
+          .getFavorites(new FavoritesParams({ userId: userId as string }))
+          .subscribe({
+            next: (favoritesList: PagedList<Beer>) => {
+              this.userFavoritesCount = favoritesList.TotalCount;
+              this.error = '';
+              this.loading = false;
+            },
+            error: () => {
+              this.error =
+                'An error occurred while loading the user favorites count';
+              this.loading = false;
+            }
+          });
+        this.opinionsSubscription = this.opinionsService
+          .getOpinions(new OpinionsParams({ userId: userId as string }))
+          .subscribe({
+            next: (opinionsList: PagedList<Opinion>) => {
+              this.userBeersRatedCount = opinionsList.TotalCount;
+              this.error = '';
+              this.loading = false;
+            },
+            error: () => {
+              this.error =
+                'An error occurred while loading the user beers rated count';
+              this.loading = false;
+            }
+          });
+      });
+  }
+  ngOnDestroy(): void {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+    if (this.favoritesSubscription) {
+      this.favoritesSubscription.unsubscribe();
+    }
+    if (this.opinionsSubscription) {
+      this.opinionsSubscription.unsubscribe();
+    }
+  }
+}
