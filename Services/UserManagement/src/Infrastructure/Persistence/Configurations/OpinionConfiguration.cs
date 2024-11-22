4bb6e71497992346dabbd68c3d9e9c758f7fff1d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-11-22 13:25:50 +0100

    Add Created column to AspNetUsers table

---
+++ Services/UserManagement/src/Infrastructure/Persistence/Configurations/OpinionConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+/// <summary>
+///     The application user entity configuration.
+/// </summary>
+public class OpinionConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    /// <summary>
+    ///     Configures the application user entity.
+    /// </summary>
+    /// <param name="builder">The builder</param>
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.Property(x => x.Created).IsRequired().HasMaxLength(50);
+    }
+}
--- Services/UserManagement/src/Infrastructure/Persistence/Migrations/20241122122019_AddedCreatedColumnToAspNetUsersTable.Designer.cs
+++ Services/UserManagement/src/Infrastructure/Persistence/Migrations/20241122122019_AddedCreatedColumnToAspNetUsersTable.Designer.cs
@@ -5,6 +5,7 @@ using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -13,9 +14,11 @@ namespace Infrastructure.Persistence.Migrations
 {
     [ExcludeFromCodeCoverage]
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241122122019_AddedCreatedColumnToAspNetUsersTable")]
+    partial class AddedCreatedColumnToAspNetUsersTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
