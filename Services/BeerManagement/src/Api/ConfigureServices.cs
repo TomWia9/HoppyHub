def423afc38f575c331ea1ab55816b61ac21ecf6
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-11-28 12:04:37 +0100

    Update MicroElements.Swashbuckle.FluentValidation package

--- Services/BeerManagement/src/Api/ConfigureServices.cs
+++ Services/BeerManagement/src/Api/ConfigureServices.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using Api.Filters;
 using FluentValidation.AspNetCore;
+using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.OpenApi.Models;
 using Serilog;
 using SharedUtilities.Interfaces;
@@ -25,12 +26,11 @@ public static class ConfigureServices
         services.AddEndpointsApiExplorer();
         services.AddHttpContextAccessor();
         services.AddFluentValidationClientsideAdapters();
-        // services.AddFluentValidationRulesToSwagger(); //TODO: Wait for .NET 9 support.
+        services.AddFluentValidationRulesToSwagger(); 
         services.AddCors(options =>
         {
             options.AddPolicy("UIApp", builder =>
             {
-                Log.Information($"CORS: {configuration.GetValue<string>("UIAppUrl")}");
                 builder.WithOrigins(configuration.GetValue<string>("UIAppUrl") ?? throw new InvalidOperationException())
                     .AllowAnyHeader()
                     .AllowAnyMethod().WithExposedHeaders("X-Pagination");
