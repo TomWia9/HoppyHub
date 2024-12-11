2a7a50aa04df9e5b7a744205567bbbfb1a4e699a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-12-11 17:14:28 +0100

    Add admin panel navigation

--- Apps/hoppy-hub/src/app/admin-panel/admin-panel.component.ts
+++ Apps/hoppy-hub/src/app/admin-panel/admin-panel.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule],
   templateUrl: './admin-panel.component.html'
 })
 export class AdminPanelComponent {}
