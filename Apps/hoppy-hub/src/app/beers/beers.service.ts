d2b185e06b1e39f115d1fda480846c2970371a7a
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2024-12-16 17:44:20 +0100

    Add more methods to beers service

--- Apps/hoppy-hub/src/app/beers/beers.service.ts
+++ Apps/hoppy-hub/src/app/beers/beers.service.ts
@@ -6,6 +6,7 @@ import { environment } from '../../environments/environment';
 import { PagedList } from '../shared/paged-list';
 import { Pagination } from '../shared/pagination';
 import { BeersParams } from './beers-params';
+import { UpsertBeerCommand } from './upsert-beer-command.model';
 
 @Injectable({
   providedIn: 'root'
@@ -51,5 +52,56 @@ export class BeersService {
           );
         })
       );
+  }
+
+  CreateBeer(upsertBeerCommand: UpsertBeerCommand): Observable<Beer> {
+    const formData = this.buildFormData(upsertBeerCommand);
+
+    return this.http.post<Beer>(
+      `${environment.beerManagementApiUrl}/beers`,
+      formData
+    );
+  }
+
+  UpdateBeer(
+    beerId: string,
+    upsertBeerCommand: UpsertBeerCommand
+  ): Observable<void> {
+    const formData = this.buildFormData(upsertBeerCommand);
+
+    return this.http.put<void>(
+      `${environment.beerManagementApiUrl}/beers/${beerId}`,
+      formData
+    );
+  }
+
+  DeleteBeer(beerId: string): Observable<void> {
+    return this.http.delete<void>(
+      `${environment.beerManagementApiUrl}/beers/${beerId}`
+    );
+  }
+
+  private buildFormData(upsertBeerCommand: UpsertBeerCommand): FormData {
+    const formData = new FormData();
+    if (upsertBeerCommand.id) {
+      formData.append('Id', upsertBeerCommand.id);
+    }
+    formData.append('Name', upsertBeerCommand.name);
+    formData.append('BreweryId', upsertBeerCommand.breweryId);
+    formData.append(
+      'AlcoholByVolume',
+      upsertBeerCommand.alcoholByVolume.toString()
+    );
+    formData.append('Description', upsertBeerCommand.description);
+    formData.append('Composition', upsertBeerCommand.composition);
+    formData.append('Blg', upsertBeerCommand.blg.toString());
+    formData.append('BeerStyleId', upsertBeerCommand.beerStyleId);
+    formData.append('Ibu', upsertBeerCommand.ibu.toString());
+    formData.append(
+      'releaseDate',
+      upsertBeerCommand.releaseDate.toDateString()
+    );
+
+    return formData;
   }
 }
