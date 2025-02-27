30c156c5eddc633958c9ff52bd3edcfe219eab7e
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2025-02-27 10:55:38 +0100

    Fix

--- Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinion-management.component.ts
+++ Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinion-management.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { OpinionsListFiltersComponent } from './opinions-list-filters/opinions-list-filters.component';
 import { OpinionsListComponent } from './opinions-list/opinions-list.component';
 
 @Component({
   selector: 'app-opinion-management',
   standalone: true,
-  imports: [OpinionsListFiltersComponent, OpinionsListComponent],
+  imports: [OpinionsListComponent],
   templateUrl: './opinion-management.component.html'
 })
 export class OpinionManagementComponent {}
--- Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinions-list/opinions-list.component.ts
+++ Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinions-list/opinions-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { OpinionsListFiltersComponent } from './opinions-list-filters/opinions-list-filters.component';
 
 @Component({
   selector: 'app-opinions-list',
   standalone: true,
-  imports: [],
+  imports: [OpinionsListFiltersComponent],
   templateUrl: './opinions-list.component.html'
 })
 export class OpinionsListComponent {}
