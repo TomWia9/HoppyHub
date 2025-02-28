e29a92f2cdbe0b28a13362ddd15a4a6620109469
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2025-02-28 11:08:38 +0100

    Add filters

--- Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinions-list/opinions-list-filters/opinions-list-filters.component.ts
+++ Apps/hoppy-hub/src/app/admin-panel/opinion-management/opinions-list/opinions-list-filters/opinions-list-filters.component.ts
@@ -1,9 +1,109 @@
-import { Component } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { OpinionsParams } from '../../../../opinions/opinions-params';
+import { OpinionsService } from '../../../../opinions/opinions.service';
+import { Pagination } from '../../../../shared/pagination';
+import { PaginationComponent } from '../../../../shared-components/pagination/pagination.component';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-opinions-list-filters',
   standalone: true,
-  imports: [],
+  imports: [
+    PaginationComponent,
+    FormsModule,
+    FontAwesomeModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './opinions-list-filters.component.html'
 })
-export class OpinionsListFiltersComponent {}
+export class OpinionsListFiltersComponent {
+  @Input({ required: true }) opinionsParams!: OpinionsParams;
+  @Input({ required: true }) paginationData!: Pagination;
+
+  private opinionsService: OpinionsService = inject(OpinionsService);
+
+  selectedSortOptionIndex: number = 0;
+  sortOptions = OpinionsParams.sortOptions;
+  onlyOpinionsWithImages: boolean = false;
+  onlyOpinionsWithComments: boolean = false;
+  faX = faX;
+  searchForm: FormGroup = new FormGroup({
+    search: new FormControl('')
+  });
+
+  onShowOnlyOpinionsWithImages(): void {
+    if (
+      this.opinionsService.paramsChanged.value.hasImage !==
+      this.onlyOpinionsWithImages
+    ) {
+      this.opinionsParams.hasImage =
+        this.onlyOpinionsWithImages == false
+          ? undefined
+          : this.onlyOpinionsWithImages;
+      this.opinionsService.paramsChanged.next(this.opinionsParams);
+    }
+  }
+
+  onShowOnlyOpinionsWithComments(): void {
+    if (
+      this.opinionsService.paramsChanged.value.hasComment !==
+      this.onlyOpinionsWithComments
+    ) {
+      this.opinionsParams.hasComment =
+        this.onlyOpinionsWithComments == false
+          ? undefined
+          : this.onlyOpinionsWithComments;
+      this.opinionsService.paramsChanged.next(this.opinionsParams);
+    }
+  }
+
+  onSort(): void {
+    this.opinionsParams.sortBy =
+      this.sortOptions[this.selectedSortOptionIndex].value;
+    this.opinionsParams.sortDirection =
+      this.sortOptions[this.selectedSortOptionIndex].direction;
+    this.opinionsService.paramsChanged.next(this.opinionsParams);
+  }
+
+  onSearch(): void {
+    if (
+      this.opinionsService.paramsChanged.value.searchQuery !==
+      this.searchForm.value.search
+    ) {
+      this.opinionsParams = new OpinionsParams({
+        pageSize: 10,
+        pageNumber: 1,
+        sortBy: 'created',
+        sortDirection: 1,
+        searchQuery: this.searchForm.value.search
+      });
+      this.opinionsService.paramsChanged.next(this.opinionsParams);
+    }
+  }
+
+  onFiltersClear(): void {
+    this.searchForm.reset();
+    this.onlyOpinionsWithImages = false;
+    this.onlyOpinionsWithComments = false;
+    this.opinionsParams = new OpinionsParams({
+      pageSize: 10,
+      pageNumber: 1,
+      sortBy: 'created',
+      sortDirection: 1
+    });
+
+    if (
+      JSON.stringify(this.opinionsService.paramsChanged.value) !=
+      JSON.stringify(this.opinionsParams)
+    ) {
+      this.opinionsService.paramsChanged.next(this.opinionsParams);
+    }
+  }
+}
