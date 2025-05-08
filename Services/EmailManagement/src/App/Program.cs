dbd5f01deea343c369ee14946e6d3b4e9a0bfedc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2025-05-08 21:19:00 +0200

    Add serilog

--- Services/EmailManagement/src/App/Program.cs
+++ Services/EmailManagement/src/App/Program.cs
@@ -1,5 +1,26 @@
+using Serilog;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Services.AddSerilog();
+
 var host = builder.Build();
-host.Run();
+
+try
+{
+    Log.Information("Starting HoppyHub - EmailManagement service");
+
+    host.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "HoppyHub - EmailManagement service terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
