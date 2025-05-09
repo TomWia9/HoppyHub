e1e0cc3fcbbb2bab59314080856b0a953debeaa0
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2025-05-09 07:15:49 +0200

    Add ConfigureServices method

---
+++ Services/EmailManagement/src/App/ConfigureServices.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+using MassTransit;
+using Serilog;
+using SharedUtilities.Filters;
+
+namespace App;
+
+/// <summary>
+///     The ConfigureServices class.
+/// </summary>
+public static class ConfigureServices
+{
+    /// <summary>
+    ///     Adds services.
+    /// </summary>
+    /// <param name="services">The services</param>
+    /// <param name="configuration">The configuration</param>
+    /// <param name="environment">The environment</param>
+    public static void AddServices(this IServiceCollection services, IConfiguration configuration,
+        IHostEnvironment environment)
+    {
+        services.AddSerilog();
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumers(Assembly.GetExecutingAssembly());
+
+            if (environment.IsDevelopment())
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(configuration.GetValue<string>("RabbitMQ:Host"), "/", h =>
+                    {
+                        h.Username(configuration.GetValue<string>("RabbitMQ:Username") ?? "");
+                        h.Password(configuration.GetValue<string>("RabbitMQ:Password") ?? "");
+                    });
+
+                    cfg.ConfigureEndpoints(context,
+                        endpointNameFormatter: new DefaultEndpointNameFormatter(prefix: "BeerManagement"));
+                    cfg.UseConsumeFilter(typeof(MessageValidationFilter<>), context);
+                });
+            }
+            else
+            {
+                x.UsingAzureServiceBus((context, cfg) =>
+                {
+                    cfg.Host(configuration.GetConnectionString("AzureServiceBusConnection"));
+                    cfg.ConfigureEndpoints(context,
+                        endpointNameFormatter: new DefaultEndpointNameFormatter(prefix: "BeerManagement"));
+                    cfg.UseConsumeFilter(typeof(MessageValidationFilter<>), context);
+                });
+            }
+        });
+        if (!environment.IsDevelopment())
+        {
+            services.AddOpenTelemetry().UseAzureMonitor();
+        }
+    }
+}
--- Services/EmailManagement/src/App/Program.cs
+++ Services/EmailManagement/src/App/Program.cs
@@ -1,8 +1,6 @@
-using System.Reflection;
+using App;
 using Azure.Identity;
-using MassTransit;
 using Serilog;
-using SharedUtilities.Filters;
 
 var builder = Host.CreateApplicationBuilder(args);
 
@@ -10,37 +8,7 @@ Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(builder.Configuration)
     .CreateLogger();
 
-builder.Services.AddSerilog();
-builder.Services.AddMassTransit(x =>
-{
-    x.AddConsumers(Assembly.GetExecutingAssembly());
-
-    if (builder.Environment.IsDevelopment())
-    {
-        x.UsingRabbitMq((context, cfg) =>
-        {
-            cfg.Host(builder.Configuration.GetValue<string>("RabbitMQ:Host"), "/", h =>
-            {
-                h.Username(builder.Configuration.GetValue<string>("RabbitMQ:Username") ?? "");
-                h.Password(builder.Configuration.GetValue<string>("RabbitMQ:Password") ?? "");
-            });
-
-            cfg.ConfigureEndpoints(context,
-                endpointNameFormatter: new DefaultEndpointNameFormatter(prefix: "BeerManagement"));
-            cfg.UseConsumeFilter(typeof(MessageValidationFilter<>), context);
-        });
-    }
-    else
-    {
-        x.UsingAzureServiceBus((context, cfg) =>
-        {
-            cfg.Host(builder.Configuration.GetConnectionString("AzureServiceBusConnection"));
-            cfg.ConfigureEndpoints(context,
-                endpointNameFormatter: new DefaultEndpointNameFormatter(prefix: "BeerManagement"));
-            cfg.UseConsumeFilter(typeof(MessageValidationFilter<>), context);
-        });
-    }
-});
+builder.Services.AddServices(builder.Configuration, builder.Environment);
 
 var host = builder.Build();
 
